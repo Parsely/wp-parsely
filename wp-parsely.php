8f342228a1d9c9a338a7d12dfa41c146d92a7ff3
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-09-22 16:37:24 -0400

    add conditional in case there are no custom taxonomies (#52)
    * add conditional in case there are no custom taxonomies
    
    * check if taxonomy is array

--- wp-parsely.php
+++ wp-parsely.php
@@ -724,10 +724,14 @@ class Parsely {
         // filter out default WordPress taxonomies
         $all_taxonomies = array_diff(get_taxonomies(), array('post_tag', 'nav_menu', 'author', 'link_category', 'post_format'));
         $all_values = array();
-        foreach ( $all_taxonomies as $taxonomy ) {
-            $custom_taxonomy_objects = get_the_terms($postObj->ID, $taxonomy);
-            foreach ( $custom_taxonomy_objects as $custom_taxonomy_object ) {
-                array_push($all_values, $custom_taxonomy_object->name);
+        if (is_array($all_taxonomies)) {
+            foreach ( $all_taxonomies as $taxonomy ) {
+                $custom_taxonomy_objects = get_the_terms($postObj->ID, $taxonomy);
+                if (is_array($custom_taxonomy_objects)) {
+                    foreach ( $custom_taxonomy_objects as $custom_taxonomy_object ) {
+                        array_push($all_values, $custom_taxonomy_object->name);
+                    }
+                }
             }
         }
         return $all_values;
