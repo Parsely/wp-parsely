e2ed4dcacd73cc8cb8d9f3ecca2636bac1fdfa00
Alexander Lourenco <alexander.lourenco@gmail.com>
2017-10-13 11:49:00 +0200

    add uuid tracking

--- parsely-javascript.php
+++ parsely-javascript.php
@@ -1,6 +1,21 @@
 <?php if(!isset($parselyOptions['apikey']) || empty($parselyOptions['apikey'])) { return; } ?>
 
 <!-- START Parse.ly Include: Standard -->
+<script>
+    PARSELY = {
+        onload: function() {
+            var rootUrl = 'https://api.parsely.com/v2/profile?apikey=<?php echo esc_html($parselyOptions["apikey"]); ?>';
+            var uuid = '&uuid=' + PARSELY.config.parsely_site_uuid;
+            var requestUrl = rootUrl + uuid + '&url=' + window.location.href;
+            console.log(requestUrl);
+            jQuery.ajax({
+                url: requestUrl,
+                dataType: "jsonp",
+            });
+        }
+    }
+</script>
+
 <div id="parsely-root" style="display: none">
   <div id="parsely-cfg" data-parsely-site="<?php echo esc_html($parselyOptions["apikey"]); ?>"></div>
 </div>
@@ -15,4 +30,5 @@
   e.setAttribute('data-cfasync', 'false'); e.src = h+"//"+u+"/p.js"; r.appendChild(e);
 })("script", "parsely", document);
 </script>
+
 <!-- END Parse.ly Include: Standard -->
