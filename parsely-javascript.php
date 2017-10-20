49f304836652b550df3a4d4d38f5b8a4db8bfd6b
Alexander Lourenco <alexander.lourenco@gmail.com>
2017-10-20 10:59:04 +0200

    respect prior modifications to PARSELY object if they exist

--- parsely-javascript.php
+++ parsely-javascript.php
@@ -1,33 +1,56 @@
 <?php if(!isset($parselyOptions['apikey']) || empty($parselyOptions['apikey'])) { return; } ?>
 
 <!-- START Parse.ly Include: Standard -->
-<script>
-    PARSELY = {
-        onload: function() {
-            var rootUrl = 'https://api.parsely.com/v2/profile?apikey=<?php echo esc_html($parselyOptions["apikey"]); ?>';
-            var uuid = '&uuid=' + PARSELY.config.parsely_site_uuid;
-            var requestUrl = rootUrl + uuid + '&url=' + window.location.href;
-            jQuery.ajax({
-                url: requestUrl,
-                dataType: "jsonp"
-            });
+<script data-cfasync="false">
+
+    function uuidProfileCall() {
+        var rootUrl = 'https://api.parsely.com/v2/profile?apikey=<?php echo esc_html($parselyOptions["apikey"]); ?>';
+        var uuid = '&uuid=' + PARSELY.config.parsely_site_uuid;
+        var requestUrl = rootUrl + uuid + '&url=' + window.location.href;
+        jQuery.ajax({
+            url: requestUrl,
+            dataType: "jsonp"
+        });
+    }
+
+    if (typeof PARSELY == 'object') {
+        var oldonload = PARSELY.onload;
+        if (typeof PARSELY.onload != 'function')
+        {
+            PARSELY.onload = uuidProfileCall
+        }
+        else
+        {
+            PARSELY.onload = function() {
+                if (oldonload)
+                {
+                    oldonload();
+                }
+                uuidProfileCall();
+            };
+        }
+    }
+
+    else {
+        PARSELY = {
+            onload: uuidProfileCall
         }
     }
 </script>
 
 <div id="parsely-root" style="display: none">
-  <div id="parsely-cfg" data-parsely-site="<?php echo esc_html($parselyOptions["apikey"]); ?>"></div>
+    <div id="parsely-cfg" data-parsely-site="<?php echo esc_html($parselyOptions["apikey"]); ?>"></div>
 </div>
 <script data-cfasync="false">
-(function(s, p, d) {
-  var h=d.location.protocol, i=p+"-"+s,
-      e=d.getElementById(i), r=d.getElementById(p+"-root"),
-      u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net"
-      :"static."+p+".com";
-  if (e) return;
-  e = d.createElement(s); e.id = i; e.async = true;
-  e.setAttribute('data-cfasync', 'false'); e.src = h+"//"+u+"/p.js"; r.appendChild(e);
-})("script", "parsely", document);
+    (function(s, p, d) {
+        var h=d.location.protocol, i=p+"-"+s,
+            e=d.getElementById(i), r=d.getElementById(p+"-root"),
+            u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net"
+                :"static."+p+".com";
+        if (e) return;
+        e = d.createElement(s); e.id = i; e.async = true;
+        e.setAttribute('data-cfasync', 'false'); e.src = h+"//"+u+"/p.js"; r.appendChild(e);
+    })("script", "parsely", document);
 </script>
 
 <!-- END Parse.ly Include: Standard -->
