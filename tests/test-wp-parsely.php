64e803602a89d940c77997348b4408d655dec706
Alexander Lourenco <alexander.lourenco@gmail.com>
2017-10-20 11:32:53 +0200

    use heredoc in tests instead of gross escaping

--- tests/test-wp-parsely.php
+++ tests/test-wp-parsely.php
@@ -81,19 +81,22 @@ class SampleTest extends WP_UnitTestCase {
             'custom_taxonomy_section' => 'category',
             'lowercase_tags' => true);
         update_option('parsely', $optionDefaults);
-        self::$parsely_html = "<div id=\"parsely-root\" style=\"display: none\">
-  <div id=\"parsely-cfg\" data-parsely-site=\"blog.parsely.com\"></div>
+        self::$parsely_html = <<<PARSELYJS
+<div id="parsely-root" style="display: none">
+    <div id="parsely-cfg" data-parsely-site="blog.parsely.com"></div>
 </div>
-<script data-cfasync=\"false\">
-(function(s, p, d) {
-  var h=d.location.protocol, i=p+\"-\"+s,
-      e=d.getElementById(i), r=d.getElementById(p+\"-root\"),
-      u=h===\"https:\"?\"d1z2jf7jlzjs58.cloudfront.net\"
-      :\"static.\"+p+\".com\";
-  if (e) return;
-  e = d.createElement(s); e.id = i; e.async = true;
-  e.setAttribute('data-cfasync', 'false'); e.src = h+\"//\"+u+\"/p.js\"; r.appendChild(e);
-})(\"script\", \"parsely\", document);";
+<script data-cfasync="false">
+    (function(s, p, d) {
+        var h=d.location.protocol, i=p+"-"+s,
+            e=d.getElementById(i), r=d.getElementById(p+"-root"),
+            u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net"
+                :"static."+p+".com";
+        if (e) return;
+        e = d.createElement(s); e.id = i; e.async = true;
+        e.setAttribute('data-cfasync', 'false'); e.src = h+"//"+u+"/p.js"; r.appendChild(e);
+    })("script", "parsely", document);
+</script>
+PARSELYJS;
     }
 
 
