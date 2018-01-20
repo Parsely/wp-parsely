b114c4bb374ebe6d7a1b9fac898deb963361d450
Alexander Lourenco <alexander.lourenco@gmail.com>
2018-01-20 23:29:42 +0100

    fix linting in parsely-parsely-page

--- parsely-parsely-page.php
+++ parsely-parsely-page.php
@@ -1,14 +1,15 @@
-<!-- BEGIN wp-parsely Plugin Version <?php echo esc_html(Parsely::VERSION); ?> -->
-<meta name='wp-parsely_version' id='wp-parsely_version' content='<?php echo esc_html(Parsely::VERSION); ?>' />
-<?php if (!empty($parselyPage) && isset($parselyPage["headline"])) : ?>
-   <script type="application/ld+json">
-   <?php echo json_encode($parselyPage); ?>
-   </script>
-    <?php if (isset($parselyPage["custom_metadata"])) : ?>
-        <meta name='parsely-metadata' content='<?php echo json_encode($parselyPage['custom_metadata'])?>'>
-        <?php endif; ?>
-<?php else: ?>
-    <!-- parsleyPage is not defined / has no attributes.  What kind of page are you loading? -->
+<!-- BEGIN wp-parsely Plugin Version <?php echo esc_html( Parsely::VERSION ); ?> -->
+<meta name='wp-parsely_version' id='wp-parsely_version' content='<?php echo esc_html( Parsely::VERSION ); ?>'/>
+<?php if ( ! empty( $parsely_page ) && isset( $parsely_page['headline'] ) ) : ?>
+	<script type="application/ld+json">
+	<?php echo json_encode( $parsely_page ); ?>
+
+	</script>
+	<?php if ( isset( $parsely_page['custom_metadata'] ) ) : ?>
+		<meta name='parsely-metadata' content='<?php echo json_encode( $parsely_page['custom_metadata'] ); ?>'>
+	<?php endif; ?>
+<?php else : ?>
+	<!-- parsleyPage is not defined / has no attributes.  What kind of page are you loading? -->
 <?php endif; ?>
-<!-- END wp-parsely Plugin Version <?php echo esc_html(Parsely::VERSION); ?> -->
+<!-- END wp-parsely Plugin Version <?php echo esc_html( Parsely::VERSION ); ?> -->
 <?php
