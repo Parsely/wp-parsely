d12d7d55c8f583d0af72e16d6847a0c47fdb4696
Alexander Lourenco <alexander.lourenco@gmail.com>
2018-01-23 23:01:17 +0100

    add repeated_metas output in parsely-parsely-page

--- parsely-parsely-page.php
+++ parsely-parsely-page.php
@@ -1,11 +1,40 @@
 <!-- BEGIN wp-parsely Plugin Version <?php echo esc_html( Parsely::VERSION ); ?> -->
 <meta name='wp-parsely_version' id='wp-parsely_version' content='<?php echo esc_html( Parsely::VERSION ); ?>'/>
-<?php if ( ! empty( $parsely_page ) && isset( $parsely_page['headline'] ) ) : ?>
+<?php $parsely_options['meta_type'] = 'repeated_metas'; if ( ! empty( $parsely_page ) && isset( $parsely_page['headline'] ) ) : ?>
+<?php
+if ( 'json-ld' == $parsely_options['meta_type'] ) {
+?>
 	<script type="application/ld+json">
 	<?php echo json_encode( $parsely_page ); ?>
 
 	</script>
-	<?php if ( isset( $parsely_page['custom_metadata'] ) ) : ?>
+
+	<?php
+}
+
+else {
+		$post_type = 'NewsArticle' == $parsely_page['@type'] ? 'post' : 'sectionpage';
+	?>
+		<meta name="parsely-title" content=<?php echo json_encode( $parsely_page['headline'] ); ?> />
+		<meta name="parsely-link" content=<?php echo json_encode( $parsely_page['url'] ); ?> />
+		<meta name="parsely-type" content=<?php echo json_encode( $post_type ); ?> />
+		<meta name="parsely-image-url" content=<?php echo json_encode( $parsely_page['thumbnailUrl'] ); ?> />
+		<meta name="parsely-pub-date" content=<?php echo json_encode( $parsely_page['datePublished'] ); ?> />
+		<meta name="parsely-section" content=<?php echo json_encode( $parsely_page['articleSection'] ); ?> />
+	<?php
+	foreach ( $parsely_page['author'] as $author ) {
+	?>
+	<meta name="parsely-author" content=<?php echo json_encode( $author['name'] ); ?> />
+		<?php
+	}
+	?>
+<meta name="parsely-tags" content=<?php echo json_encode( $parsely_page['keywords'] ); ?> />
+
+	<?php
+
+}
+if ( isset( $parsely_page['custom_metadata'] ) ) :
+	?>
 		<meta name='parsely-metadata' content='<?php echo json_encode( $parsely_page['custom_metadata'] ); ?>'>
 	<?php endif; ?>
 <?php else : ?>
