fb792a5b54ff53ea9e06f47cb226cf5abcbc25ee
Sean Fisher <srtfisher@gmail.com>
2018-12-13 23:48:16 -0500

    Add a check to see if the post status is actually public

--- wp-parsely.php
+++ wp-parsely.php
@@ -721,7 +721,15 @@ class Parsely {
 
 		$current_url = $this->get_current_url();
 
-		if ( in_array( get_post_type(), $parsely_options['track_post_types'], true ) && 'publish' === $post->post_status ) {
+		/**
+		 * Check if the post is a trackable post status.
+		 * 
+		 * @param bool $is_public Flag if the post status is public.
+		 * @param WP_Post $post Post object.
+		 */
+		$is_public_post_status = apply_filters( 'wp_parsely_is_public_status', get_post_status_object( $post->post_status )->public );
+
+		if ( in_array( get_post_type(), $parsely_options['track_post_types'], true ) && $is_public_post_status ) {
 			$authors  = $this->get_author_names( $post );
 			$category = $this->get_category_name( $post, $parsely_options );
 			$post_id  = $parsely_options['content_id_prefix'] . (string) get_the_ID();
