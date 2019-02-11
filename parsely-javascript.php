d91dc2c9958ed302e631cee16aafddbf369d2f8a
Josh Romero <josh@joshromero.com>
2019-02-11 12:11:35 -0600

    update tracker to config-free

--- parsely-javascript.php
+++ parsely-javascript.php
@@ -54,23 +54,6 @@ if ( ! isset( $parsely_options['apikey'] ) || empty( $parsely_options['apikey'] 
 	</script>
 <?php endif; ?>
 
-<div id="parsely-root" style="display: none">
-	<div id="parsely-cfg" data-parsely-site="<?php echo esc_attr( $parsely_options['apikey'] ); ?>"></div>
-</div>
-<script data-cfasync="false">
-	(function (s, p, d) {
-		var h = d.location.protocol, i = p + "-" + s,
-			e = d.getElementById(i), r = d.getElementById(p + "-root"),
-			u = h === "https:" ? "d1z2jf7jlzjs58.cloudfront.net"
-				: "static." + p + ".com";
-		if (e) return;
-		e = d.createElement(s);
-		e.id = i;
-		e.async = true;
-		e.setAttribute('data-cfasync', 'false');
-		e.src = h + "//" + u + "/p.js";
-		r.appendChild(e);
-	})("script", "parsely", document);
-</script>
+<script id="parsely-cfg" data-parsely-site="<?php echo esc_attr( $parsely_options['apikey'] ); ?>" src="//cdn.parsely.com/keys/<?php echo esc_attr( $parsely_options['apikey'] ); ?>/p.js"></script>
 
 <!-- END Parse.ly Include: Standard -->
