4bf2ee7c533cb4c077df52e28ab932c6ee02182a
Josh Romero <josh@joshromero.com>
2019-02-13 13:08:49 -0600

    Update to use config-free tracking (#129)
    * update tracker to config-free
    
    * update test to expect config-free tracker
    
    * update fbia integration to use config-free tracker
    
    * restore data-cfasync='false' to prevent cloudfront woes
    
    * also update test to include data-cfasync attr

--- wp-parsely.php
+++ wp-parsely.php
@@ -1373,20 +1373,7 @@ class Parsely {
 				}
 			}
 		</script>
-		<div id="parsely-root" style="display: none">
-			<span id="parsely-cfg" data-parsely-site="' . esc_attr( $options['apikey'] ) . '"></span>
-		</div>
-		<script>
-			( function(s, p, d ) {
-			var h=d.location.protocol, i=p+"-"+s,
-			e=d.getElementById( i), r=d.getElementById(p+"-root" ),
-			u=h==="https:"?"d1z2jf7jlzjs58.cloudfront.net"
-			:"static."+p+".com";
-			if ( e ) return;
-			e = d.createElement( s ); e.id = i; e.async = true;
-			e.src = h+"//"+u+"/p.js"; r.appendChild( e );
-			})( "script", "parsely", document );
-		</script>
+		<script data-cfasync="false" id="parsely-cfg" data-parsely-site="' . esc_attr( $options['apikey'] ) . '" src="//cdn.parsely.com/keys/' . esc_attr( $options['apikey'] ) . '/p.js"></script>
 		<!-- END Parse.ly Include: Standard -->';
 
 		$registry[ $identifier ] = array(
