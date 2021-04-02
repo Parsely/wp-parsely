eed7f6bf5857a752ea08a41527d3bb23a3ac2e70
Jeff Bowen <jblz@users.noreply.github.com>
2021-04-02 13:02:53 -0400

    write admin recrawl notices w/ vanilla js

--- parsely-admin-header.php
+++ parsely-admin-header.php
@@ -19,28 +19,26 @@
 	}
 </style>
 <script type="text/javascript">
-(function($) {
-	$(document).ready(function onDOMReady() {
-		var apikey = $('#apikey').val();
-		var recrawlRequiredMessage = $('<p>')
-			.addClass('description');
-		var supportLink = $('<a>',{
-			href: 'mailto:support@parsely.com?subject=Please reprocess ' + encodeURIComponent(apikey),
-			text: 'support@parsely.com'
-		});
-
-		recrawlRequiredMessage.append('<strong style="color:red;">Important:</strong>');
-
-		recrawlRequiredMessage
-			.append(' changing this value on a site currently tracked with ' +
+(function() {
+	window.addEventListener( 'DOMContentLoaded', function( event ) {
+		var keyEl = document.querySelector( '#apikey' );
+		var requiresRecrawlNotice = document.querySelectorAll( 'div.parsely-form-controls[data-requires-recrawl="true"] .help-text' );
+		if ( ! ( keyEl && requiresRecrawlNotice.length ) ) {
+			return;
+		}
+		var notice = '<p class="description">' +
+			'<strong style="color:red;">Important:</strong>' +
+			' changing this value on a site currently tracked with ' +
 			'Parse.ly will require reprocessing of your Parse.ly data. Once ' +
-			'you have changed this value, please contact ' );
-
-		recrawlRequiredMessage.append(supportLink);
-
-		recrawlRequiredMessage
-			.appendTo("div.parsely-form-controls[data-requires-recrawl='true'] .help-text");
-	});
-})(jQuery);
+			'you have changed this value, please contact ' +
+			'<a href="mailto:support@parsely.com?subject=Please reprocess ' + encodeURIComponent( keyEl.value ) + '">' +
+				'support@parsely.com'
+			'</a>' +
+		'</p>';
+		[].forEach.call( requiresRecrawlNotice, function( n ) {
+			n.innerHTML += notice;
+		} );
+	} );
+})();
 </script>
 <!-- end wp-parsely -->
