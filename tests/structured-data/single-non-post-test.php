3a9901aac5cbcac0545876ff513a3642e8bf6e96
mikeyarce <mikeyarce@gmail.com>
2021-04-06 12:56:09 -0700

    Add unit test for blog archive not on homepage
    This tests cases where the homepage is a static page and the blog archive is a separate page.  We want to make sure the blog archive page has the right metadata from the page and not from the posts in the archive.

--- tests/structured-data/single-non-post-test.php
+++ tests/structured-data/single-non-post-test.php
@@ -84,6 +84,43 @@ final class Single_Non_Post_Test extends TestCase {
 	}
 
 	/**
+	 * Create a single page, set as the posts page (blog archive) but not the home page, and test the structured data.
+	 */
+	public function test_blog_page_for_posts() {
+		// Setup Parsley object.
+		$parsely         = new \Parsely();
+		$parsely_options = get_option( \Parsely::OPTIONS_KEY );
+
+		// Insert a page for blog posts and insert another post.
+		$page_id = $this->factory()->post->create( [ 'post_type' => 'page', 'post_title' => 'Page for Posts', 'post_name' => 'page-for-posts' ] );
+		$this->factory()->post->create();
+		$page    = get_post( $page_id );
+
+		// Set permalinks, as Parsely currently strips ?page_id=... from the URL property.
+		// See https://github.com/Parsely/wp-parsely/issues/151
+		global $wp_rewrite;
+		$wp_rewrite->set_permalink_structure('/%postname%/');
+
+		// Set a static page to the homepage, set the newly created page to show the posts
+		update_option( 'show_on_front', 'page' );
+		update_option('page_on_front', 1 );
+		update_option( 'page_for_posts', $page_id );
+
+		// Make a request to the root of the site to set the global $wp_query object.
+		$this->go_to( get_permalink( $page_id ) );
+
+		// Create the structured data for that post.
+		$structured_data = $parsely->construct_parsely_metadata( $parsely_options, $page );
+
+		// Check the required properties exist.
+		$this->assert_data_has_required_properties( $structured_data );
+
+		// The headline should be the title of the post, not the name of the Site.
+		self::assertEquals( 'Page for Posts', $structured_data['headline'] );
+		self::assertEquals( get_permalink( $page_id ), $structured_data['url'] );
+	}
+
+	/**
 	 * Create a single page, set as homepage (page on front), and test the structured data.
 	 */
 	public function test_home_page_on_front() {
