ed5d42ba9a46ca483a93e4e444cd2e1bd93e68db
Gary Jones <gary@gamajo.com>
2021-04-10 01:42:57 +0100

    Tests: Add author archive metadata test
    See #155.

--- tests/structured-data/archive-post-test.php
+++ tests/structured-data/archive-post-test.php
@@ -99,6 +99,38 @@ final class Archive_Post_Test extends TestCase {
 		self::assertEquals( get_permalink( $page_id ) . 'page/2', $structured_data['url'] );
 	}
 
+	public function test_author_archive() {
+		// Set permalinks, as Parsely currently strips ?page_id=... from the URL property.
+		// See https://github.com/Parsely/wp-parsely/issues/151
+		$this->set_permalink_structure( '/%postname%/' );
+
+		// Setup Parsley object.
+		$parsely         = new \Parsely();
+		$parsely_options = get_option( \Parsely::OPTIONS_KEY );
+
+		// Insert a single user, and a Post assigned to them.
+		$user = self::factory()->user->create( [ 'user_login' => 'parsely' ] );
+		$this->factory()->post->create( [ 'post_author' => $user ] );
+
+		// Make a request to that page to set the global $wp_query object.
+		$author_posts_url = get_author_posts_url( $user );
+		$this->go_to( $author_posts_url );
+
+		// Reset permalinks to default.
+		$this->set_permalink_structure( '' );
+
+		// Create the structured data for that category.
+		// The author archive metadata doesn't use the post data, but the construction method requires it for now.
+		$structured_data = $parsely->construct_parsely_metadata( $parsely_options, get_post() );
+
+		// Check the required properties exist.
+		$this->assert_data_has_required_properties( $structured_data );
+
+		// The headline should be the category name.
+		self::assertEquals( 'Author - parsely', $structured_data['headline'] );
+		self::assertEquals( $author_posts_url, $structured_data['url'] );
+	}
+
 	public function test_term_archive() {
 		// Set permalinks, as Parsely currently strips ?page_id=... from the URL property.
 		// See https://github.com/Parsely/wp-parsely/issues/151
