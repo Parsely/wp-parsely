cf751aebddd5372572a940cb5dde99e4f310ffc0
Jeff Bowen <jblz@users.noreply.github.com>
2021-04-19 19:40:49 -0400

    Wrap the string in the admin js for translation

--- src/lib/admin-page/index.js
+++ src/lib/admin-page/index.js
@@ -1,4 +1,5 @@
 wp.domReady( () => {
+	const { sprintf } = wp.i18n;
 	const keyEl = document.querySelector( '#apikey' );
 	const requiresRecrawlNotice = document.querySelectorAll(
 		'div.parsely-form-controls[data-requires-recrawl="true"] .help-text'
@@ -7,9 +8,11 @@ wp.domReady( () => {
 		return;
 	}
 
-	const notice = `<p class="description"><strong style="color:red;">Important:</strong> changing this value on a site currently tracked with Parse.ly will require reprocessing of your Parse.ly data. Once you have changed this value, please contact <a href="mailto:support@parsely.com?subject=Please reprocess ${ encodeURIComponent(
-		keyEl.value
-	) }">support@parsely.com</a></p>`;
+	const notice = sprintf(
+		'<p class="description"><strong style="color:red;">Important:</strong> changing this value on a site currently tracked with Parse.ly will require reprocessing of your Parse.ly data. Once you have changed this value, please contact <a href="mailto:support@parsely.com?subject=Please reprocess %s">support@parsely.com</a></p>',
+		keyEl.value,
+		'wp-parsely'
+	);
 	[].forEach.call( requiresRecrawlNotice, function( n ) {
 		n.innerHTML += notice;
 	} );
