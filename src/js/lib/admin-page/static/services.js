f5d2442eacb58fabe57779a47567e2ca39479305
jase-d-ace <andrada.jasonp@gmail.com>
2021-04-22 12:57:22 -0400

    transform json into array before passing to component

--- src/js/lib/admin-page/static/services.js
+++ src/js/lib/admin-page/static/services.js
@@ -1,6 +1,13 @@
 export const fetchSettings = ( callback ) => {
 	fetch( '/wp-json/wp-parsely/v1/settings' )
 		.then( res => res.json() )
-		.then( settings => callback( settings ) )
+		.then( settings => {
+			let holderArray = [];
+			// transform json object into array of k/v pairs for component mapping
+			Object.keys(settings).forEach(setting => {
+				holderArray.push({[setting]: settings[setting]});
+			});
+			callback(holderArray);
+		} )
 		.catch( err => callback( err ) );
 };
