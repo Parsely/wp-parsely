f571199c61406a42736b018b7b098c3a59d2b02b
jase-d-ace <andrada.jasonp@gmail.com>
2021-04-22 11:18:28 -0400

    Add static folder for services file

---
+++ src/lib/admin-page/static/services.js
@@ -0,0 +1,6 @@
+export const fetchSettings = ( callback ) => {
+	fetch( '/wp-json/wp-parsely/v1/settings' )
+		.then( res => res.json() )
+		.then( settings => callback( settings ) )
+		.catch( err => callback( err ) );
+};
