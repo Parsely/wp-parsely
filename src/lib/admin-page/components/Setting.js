8127f0e0903c0235ff63acdf02392da101d553c2
jase-d-ace <andrada.jasonp@gmail.com>
2021-04-27 11:18:40 -0400

    change variable name

--- src/lib/admin-page/components/Setting.js
+++ src/lib/admin-page/components/Setting.js
@@ -2,18 +2,15 @@ import Input from './Input';
 import Select from './Select';
 
 const Setting = ( setting ) => {
-	let inputType;
+	let input;
 	if ( typeof ( setting[ Object.keys( setting )[ 0 ] ] ) === 'string' ) {
-		inputType = <Input type="text" value={setting[ Object.keys( setting )[ 0 ] ]} checked="" />;
+		input = <Input type="text" value={setting[ Object.keys( setting )[ 0 ] ]} checked="" />;
 	} else if ( typeof ( setting[ Object.keys( setting )[ 0 ] ] ) === 'boolean' ) {
-		// figure out true/false logic
-		inputType = <Input type="checkbox" checked={setting[ Object.keys( setting )[ 0 ] ]} />;
+		input = <Input type="checkbox" checked={setting[ Object.keys( setting )[ 0 ] ]} />;
 	} else if ( typeof ( setting[ Object.keys( setting )[ 0 ] ] ) === 'object' ) {
-		// figure out select/option stuff
-		inputType = <Select values={setting[ Object.keys( setting )[ 0 ] ]} />;
+		input = <Select values={ setting[ Object.keys( setting )[ 0 ] ] } />;
 	}
 
-	return <li>{ Object.keys( setting )[ 0 ] }: { inputType }</li>;
+	return <li>{ Object.keys( setting )[ 0 ] }: { input }</li>;
 };
 export default Setting;
-// <input type={ inputType } value={ setting[ Object.keys( setting )[ 0 ] ] }
