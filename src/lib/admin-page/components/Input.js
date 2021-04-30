afb72d619581c69f3dbbc154e6c0e7db40ef00c1
jase-d-ace <andrada.jasonp@gmail.com>
2021-04-30 14:55:55 -0400

    make checkboxes dynamic

--- src/lib/admin-page/components/Input.js
+++ src/lib/admin-page/components/Input.js
@@ -1,3 +1,18 @@
-const Input = ( { type, checked, value } ) => ( <input type={ type } checked={ checked } value={ value } /> );
+import { useState } from '@wordpress/element';
+
+const Input = ( { type, checked, value } ) => {
+	const [boolean, setBoolean] = useState(checked);
+	const [inputVal, setInputVal] = useState(null);
+
+	const onInputChange = e => {
+		if ( type == "checkbox" ) {
+			setBoolean(!boolean)
+		} else if ( type == "text" ) {
+			setInputVal(e.target.value)
+		}
+	}
+
+	return ( <input type={type} checked={boolean} value={value} onChange={onInputChange} /> );
+}
 
 export default Input;
