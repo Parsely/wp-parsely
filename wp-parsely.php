21b42d4246136486a70da3a5f9b492e50458cb4c
Dat Hoang <htdat@users.noreply.github.com>
2021-04-30 11:14:03 +0700

    Remove unnecessary comment

--- wp-parsely.php
+++ wp-parsely.php
@@ -1654,7 +1654,7 @@ class Parsely {
 					? sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) )
 					: '';
 
-			$url = home_url( $request_uri ); // Input var okay.
+			$url = home_url( $request_uri );
 		}
 
 		$options = $this->get_options();
