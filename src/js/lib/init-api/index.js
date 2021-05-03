55cb973efbf47ee6ec664013732e5e6ce4ffb553
Jeff Bowen <jblz@users.noreply.github.com>
2021-05-03 09:32:24 -0400

    Convert Profile API call to simple XHR - no more JSONP

--- src/js/lib/init-api/index.js
+++ src/js/lib/init-api/index.js
@@ -1,33 +1,30 @@
-function uuidProfileCall( { apikey, uuid } ) {
-	const profileScript = document.createElement( 'script' );
-	profileScript.src = `https://api.parsely.com/v2/profile?apikey=${ encodeURIComponent(
+async function uuidProfileCall() {
+	const apikey = global.wpParsely?.apikey;
+	const uuid = global.PARSELY?.config?.parsely_site_uuid;
+
+	if ( ! ( apikey && uuid ) ) {
+		return;
+	}
+
+	const url = `https://api.parsely.com/v2/profile?apikey=${ encodeURIComponent(
 		apikey
-	) }&uuid=${ uuid || '' }&url=${ window.location.href }&callback=&_=${ +new Date() }`;
-	document.querySelector( 'head' ).appendChild( profileScript );
+	) }&uuid=${ encodeURIComponent( uuid ) }&url=${ window.location.href }`;
+
+	return fetch( url );
 }
 
 export function initApi() {
-	if ( typeof global.wpParsely !== 'object' ) {
-		return;
-	}
-
-	const { apikey } = global.wpParsely;
-
-	if ( ! apikey ) {
-		return;
-	}
-
 	if ( typeof global.PARSELY === 'object' ) {
-		const oldonload = global.PARSELY.onload;
 		if ( typeof global.PARSELY.onload !== 'function' ) {
 			global.PARSELY.onload = uuidProfileCall;
 			return;
 		}
+		const oldonload = global.PARSELY.onload;
 		global.PARSELY.onload = function() {
 			if ( oldonload ) {
 				oldonload();
 			}
-			uuidProfileCall( { apikey, uuid: global.PARSELY.config.parsely_site_uuid } );
+			uuidProfileCall();
 		};
 		return;
 	}
