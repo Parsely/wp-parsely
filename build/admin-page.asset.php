f1314f9ab469e189f186143824b6624aa2e07199
Jeff Bowen <jblz@users.noreply.github.com>
2021-05-05 11:59:59 -0400

    include built assets

--- build/admin-page.asset.php
+++ build/admin-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '85a73a1a0f746e273975fe432a09361c');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'dc1cb6730e8f8db850bff3efbd5d2fb9');
