1ea6fb46b52bf43b9477aaa1cd93fee4fffef5e6
Jeff Bowen <jblz@users.noreply.github.com>
2021-05-06 21:28:01 -0400

    Improve src directory structure for widgets/recommended & keep entry slug

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,6 @@ module.exports = {
 	entry: {
 		'init-api': path.resolve( __dirname, 'src', 'js', 'lib', 'init-api' ),
 		'admin-page': path.resolve( __dirname, 'src', 'js', 'lib', 'admin-page' ),
-		'recommended-widget': path.resolve( __dirname, 'src', 'js', 'recommended-widget' ),
+		'recommended-widget': path.resolve( __dirname, 'src', 'js', 'widgets', 'recommended' ),
 	},
 };
