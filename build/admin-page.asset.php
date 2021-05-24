343ab311259ec0395f46a5893de88aa3eb2f4f69
jase-d-ace <andrada.jasonp@gmail.com>
2021-05-24 15:40:33 -0400

    build assets

--- build/admin-page.asset.php
+++ build/admin-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '48211e663e15c20bfebba6a9e44ab34b');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'be8a8a126ae2b3d858d9fe9b50fd7312');
