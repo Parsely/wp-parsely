7febe6a647a7afaf8a029f7ffd1226bf88b8fbd0
Jeff Bowen <jblz@users.noreply.github.com>
2021-06-03 16:40:55 -0400

    prettier & eslint fixes

--- src/js/lib/admin-page/components/App.js
+++ src/js/lib/admin-page/components/App.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from '@wordpress/element';
 import { fetchSettings, saveSettingsToServer } from '../../settings-api-client';
 import Setting from './Setting';
 import SiteDetails from './SiteDetails';
-import WipeMetadataCache from "./WipeMetadataCache";
-import WipeMetadataModal from "./WipeMetadataModal";
+import WipeMetadataCache from './WipeMetadataCache';
+import WipeMetadataModal from './WipeMetadataModal';
 
 const App = () => {
 	const [ settings, setSettings ] = useState( null );
-	const [currentTab, setCurrentTab] = useState( "advanced" );
-	const [displayModal, setDisplayModal] = useState(false);
+	const [ currentTab, setCurrentTab ] = useState( 'advanced' );
+	const [ displayModal, setDisplayModal ] = useState( false );
 
 	useEffect( () => {
 		fetchSettings()
@@ -26,13 +26,13 @@ const App = () => {
 		setSettings( { ...settings, [ e.target.name ]: newValue } );
 	};
 
-	const displayDiv = (divClass, currentState) => divClass === currentState ? '':'inactive';
+	const displayDiv = ( divClass, currentState ) => ( divClass === currentState ? '' : 'inactive' );
 
-	const showModal = (val) => {
-		setDisplayModal(val);
-	}
+	const showModal = ( val ) => {
+		setDisplayModal( val );
+	};
 
-	const setMetadataFlag = (val) => setSettings({...settings, parsely_wipe_metadata_cache: val})
+	const setMetadataFlag = ( val ) => setSettings( { ...settings, parsely_wipe_metadata_cache: val } );
 
 	const handleFormSubmit = async ( e ) => {
 		e.preventDefault();
@@ -48,143 +48,140 @@ const App = () => {
 	return (
 		<div className="settings-container">
 			<nav className="controls">
-				<div className="nav-control"  onClick={() => setCurrentTab("general")}>
-					<span className={`${currentTab === "general" ? "active": ''}`}>
-						General
-					</span>
+				<div className="nav-control" onClick={ () => setCurrentTab( 'general' ) }>
+					<span className={ `${ currentTab === 'general' ? 'active' : '' }` }>General</span>
 				</div>
-				<div className="nav-control" onClick={() => setCurrentTab("advanced")}>
-					<span className={`${currentTab === "advanced" ? "active": ''}`}>
-						Advanced
-					</span>
+				<div className="nav-control" onClick={ () => setCurrentTab( 'advanced' ) }>
+					<span className={ `${ currentTab === 'advanced' ? 'active' : '' }` }>Advanced</span>
 				</div>
-				<div className="nav-control" onClick={() => setCurrentTab("debug")}>
-					<span className={`${currentTab === "debug" ? "active": ''}`}>
-						Debug
-					</span>
+				<div className="nav-control" onClick={ () => setCurrentTab( 'debug' ) }>
+					<span className={ `${ currentTab === 'debug' ? 'active' : '' }` }>Debug</span>
 				</div>
-
 			</nav>
 			<form className="settings-form" onSubmit={ ( e ) => handleFormSubmit( e ) }>
 				{ settings ? (
 					<div className="settings-holder">
-						<div className={`tab-body general ${displayDiv("general", currentTab)}`}>
+						<div className={ `tab-body general ${ displayDiv( 'general', currentTab ) }` }>
 							<Setting
-								setting={{apikey: settings["apikey"]}}
+								setting={ { apikey: settings.apikey } }
 								label="Site ID"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Your SiteID is your own site domain"
 							/>
 							<Setting
-								setting={{apiSecret: "no secret given"}}
+								setting={ { apiSecret: 'no secret given' } }
 								label="API Secret"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Your API Secret is your secret code to access our API"
 							/>
 							<Setting
-								setting={{logo: settings["logo"]}}
+								setting={ { logo: settings.logo } }
 								label="Logo"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="You can pass a URL to set your site's logo"
 							/>
 							<Setting
-								setting={{track_post_types: settings["track_post_types"]}}
+								setting={ { track_post_types: settings.track_post_types } }
 								label="Track Post Types"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="You can pass a URL to set your site's logo"
 							/>
 							<Setting
-								setting={{track_page_types: settings["track_page_types"]}}
+								setting={ { track_page_types: settings.track_page_types } }
 								label="Track Page Types"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="You can pass a URL to set your site's logo"
 							/>
 						</div>
-						<div className={`tab-body advanced ${displayDiv("advanced", currentTab)}`}>
+						<div className={ `tab-body advanced ${ displayDiv( 'advanced', currentTab ) }` }>
 							<Setting
-								setting={{meta_type: settings["meta_type"]}}
+								setting={ { meta_type: settings.meta_type } }
 								label="Metadata Type"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Choose the metadata format for us to track"
 							/>
 							<Setting
-								setting={{custom_taxonomy_section: settings["custom_taxonomy_section"]}}
+								setting={ { custom_taxonomy_section: settings.custom_taxonomy_section } }
 								label="Custom Taxonomy Section"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: Category. Choose the default taxonomy to map to Parse.ly sections"
 							/>
 							<Setting
-								setting={{content_id_prefix: settings["content_id_prefix"]}}
+								setting={ { content_id_prefix: settings.content_id_prefix } }
 								label="Content ID Prefix"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Choose a custom prefix for your content"
 							/>
 							<Setting
-								setting={{disable_javascript: settings["disable_javascript"]}}
+								setting={ { disable_javascript: settings.disable_javascript } }
 								label="Disable Javascript"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: Off. Disable our javascript tracking if you use a separate system for JS tracking"
 							/>
 							<Setting
-								setting={{disable_amp: settings["disable_amp"]}}
+								setting={ { disable_amp: settings.disable_amp } }
 								label="Disable AMP"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: On. Disable our AMP tracking if you use a separate system to track AMP content"
 							/>
 							<Setting
-								setting={{use_top_level_cats: settings["use_top_level_cats"]}}
+								setting={ { use_top_level_cats: settings.use_top_level_cats } }
 								label="Use Top-Level Categories"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: On. Choose if you want the first top-level category to be mapped to Parse.ly"
 							/>
 							<Setting
-								setting={{cats_as_tags: settings["cats_as_tags"]}}
+								setting={ { cats_as_tags: settings.cats_as_tags } }
 								label="Categories as Tags"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: On. Choose if you want your non-primary categories to appear as tags"
 							/>
 							<Setting
-								setting={{track_authenticated_users: settings["track_authenticated_users"]}}
+								setting={ { track_authenticated_users: settings.track_authenticated_users } }
 								label="Track Authenticated Users"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: On."
 							/>
 							<Setting
-								setting={{lowercase_tags: settings["lowercase_tags"]}}
+								setting={ { lowercase_tags: settings.lowercase_tags } }
 								label="Lowercase Tags"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: On. Choose if you want your tags to be converted to lower case"
 							/>
 							<Setting
-								setting={{force_https_canonicals: settings["force_https_canonicals"]}}
+								setting={ { force_https_canonicals: settings.force_https_canonicals } }
 								label="Force HTTPS Canonical URLs"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="Default: Off. Choose if you want your canonicals to use the HTTPS scheme"
 							/>
 						</div>
-						<div className={`tab-body debug ${displayDiv("debug", currentTab)}`}>
+						<div className={ `tab-body debug ${ displayDiv( 'debug', currentTab ) }` }>
 							<Setting
-								setting={{metadata_secret: settings["metadata_secret"]}}
+								setting={ { metadata_secret: settings.metadata_secret } }
 								label="Metadata Secret"
-								onChange={handleInputChange}
+								onChange={ handleInputChange }
 								note="The metadata secret provided to you by Parse.ly"
 							/>
 							<WipeMetadataCache
-								setting={{parsely_wipe_metadata_cache: settings["parsely_wipe_metadata_cache"]}}
+								setting={ { parsely_wipe_metadata_cache: settings.parsely_wipe_metadata_cache } }
 								label="Wipe Metadata Cache"
-								onClick={showModal}
+								onClick={ showModal }
 								note="This will wipe all of your site's metadata and resend all metadata to Parse.ly"
 							/>
-							{ displayModal ? <WipeMetadataModal
-							onConfirm={setMetadataFlag}
-							apikey={settings["apikey"]}
-							modalControl={showModal}
-							setting={{parsely_wipe_metadata_cache: settings["parsely_wipe_metadata_cache"]}}
-							/> : ''}
+							{ displayModal ? (
+								<WipeMetadataModal
+									onConfirm={ setMetadataFlag }
+									apikey={ settings.apikey }
+									modalControl={ showModal }
+									setting={ { parsely_wipe_metadata_cache: settings.parsely_wipe_metadata_cache } }
+								/>
+							) : (
+								''
+							) }
 							<SiteDetails
-								apikey={settings["apikey"]}
-								postsToTrack={settings["track_post_types"]}
-								pagesToTrack={settings["track_page_types"]}
+								apikey={ settings.apikey }
+								postsToTrack={ settings.track_post_types }
+								pagesToTrack={ settings.track_page_types }
 								pluginVersion="2.5"
 								phpVersion="7.4.1"
 							/>
