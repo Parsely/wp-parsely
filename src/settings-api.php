1314b5698283071f1ba31da95e54f4728c602bf9
Jeff Bowen <jblz@users.noreply.github.com>
2021-06-03 15:12:16 -0400

    WP_REST_Response all the things

--- src/settings-api.php
+++ src/settings-api.php
@@ -39,10 +39,10 @@ function wp_parsely_settings_api_write_settings( WP_REST_Request $request ) {
 	$validated_settings = $parsely->validate_options( $new_settings );
 
 	if ( ! empty( $wp_settings_errors ) ) {
-		wp_send_json_error( $wp_settings_errors, 400 );
+		return new WP_REST_Response( array( 'errors' => $wp_settings_errors ), 400 );
 	}
 
-	wp_send_json_success( $validated_settings );
+	return new WP_REST_Response( $validated_settings, 200 );
 }
 
 function wp_parsely_settings_api_init() {
