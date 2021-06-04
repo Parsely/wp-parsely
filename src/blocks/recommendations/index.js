9aefa440250cd8acab6fb0a6a832b057d3531fcb
Jeff Bowen <jblz@users.noreply.github.com>
2021-06-04 17:30:25 -0400

    print recommendations on front end in an unordered list

--- src/blocks/recommendations/index.js
+++ src/blocks/recommendations/index.js
@@ -8,41 +8,49 @@ import { render, useEffect, useState } from '@wordpress/element';
  */
 import { fetchRelated } from '../../js/lib/parsely-api';
 
-function ParselyRecommendations( { attributes } ) {
+function ParselyRecommendations() {
 	const [ error, setError ] = useState( null );
 	const [ isLoaded, setIsLoaded ] = useState( false );
-	const [ recommendations, setRecommendations ] = useState( null );
+	const [ recommendations, setRecommendations ] = useState( [] );
 
-	useEffect( () => {
-		async function fetchRecos() {
-			let data = [];
-			try {
-				const response = await fetchRelated();
-				data = response?.data;
-			} catch ( e ) {
-				setError( e );
-			}
-			setIsLoaded( true );
-			setRecommendations( data );
+	async function fetchRecos() {
+		let data = [];
+		try {
+			const response = await fetchRelated();
+			data = response?.data;
+		} catch ( e ) {
+			setError( e );
 		}
-		fetchRecos();
-	}, [] );
+		setIsLoaded( true );
+		setRecommendations( data );
+	}
+
+	useEffect( fetchRecos, [] );
+
+	if ( ! isLoaded ) {
+		return <>Loading...</>; // TODO improve
+	}
+
+	if ( error ) {
+		return <>{ error }</>; // TODO improve
+	}
+
+	if ( ! recommendations.length ) {
+		return <>No recommendations :(</>; // TODO improve
+	}
 
 	return (
-		<div>
-			ohai, hullo!
-			<br />
-			{ JSON.stringify( {
-				attributes,
-				recommendations,
-				isLoaded,
-				error,
-			} ) }
-		</div>
+		<ul className="parsely-recommendations__linklist">
+			{ recommendations.map( ( { title, url }, index ) => (
+				<li key={ index }>
+					<a href={ url }>{ title }</a>
+				</li>
+			) ) }
+		</ul>
 	);
 }
 
-render(
-	<ParselyRecommendations />,
-	document.getElementById( 'wp-parsely-related-posts' ).querySelector( '.related-posts__grid' )
-);
+const el = document.getElementById( 'wp-parsely-related-posts' ).querySelector( '.container' );
+if ( el ) {
+	render( <ParselyRecommendations />, el );
+}
