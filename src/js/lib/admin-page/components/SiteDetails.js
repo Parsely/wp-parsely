9656d920474c584bcfa3635b886997d4a0e78c78
jase-d-ace <andrada.jasonp@gmail.com>
2021-06-12 19:16:43 -0400

    add copy to clipboard functionality to SiteDetail component

--- src/js/lib/admin-page/components/SiteDetails.js
+++ src/js/lib/admin-page/components/SiteDetails.js
@@ -1,29 +1,42 @@
-const SiteDetails = ( { apikey, postsToTrack, pagesToTrack, phpVersion, pluginVersion } ) => (
-	<div className="site-details-container">
-		<div className="site-detail">
-			<div className="details-label">
-				<span>Site Details</span>
-			</div>
-			<div className="details-info">
-				<span className="details-info-item">
-					Parsely Site ID: { apikey }
-				</span>
-				<span className="details-info-item">
-					PHP Version: { phpVersion }
-				</span>
-				<span className="details-info-item">
-					Post Types to Track: { postsToTrack }
-				</span>
-				<span className="details-info-item">
-					Page Types to Track: { pagesToTrack }
-				</span>
-				<span className="details-info-item">
-					Plugin Version: { pluginVersion }
-				</span>
-				<p className="copy-cta">Copy to Clipboard</p>
+const SiteDetails = ( { apikey, postsToTrack, pagesToTrack, phpVersion, pluginVersion } ) => {
+	const copyToClipboard = () => {
+		const el = document.createElement( 'textarea' );
+		el.value = `Site ID: ${ apikey }, PHP Version: ${ phpVersion }, Post Types to track: ${ postsToTrack }, Pages to Track: ${ pagesToTrack }, Plugin Version: ${ pluginVersion }`;
+		el.setAttribute( 'readonly', '' );
+		el.style.position = 'absolute';
+		el.style.left = '-9999px';
+		document.body.appendChild( el );
+		el.select();
+		document.execCommand( 'copy' );
+		document.body.removeChild( el );
+	};
+	return (
+		<div className="site-details-container">
+			<div className="site-detail">
+				<div className="details-label">
+					<span>Site Details</span>
+				</div>
+				<div className="details-info">
+					<span className="details-info-item">
+						Parsely Site ID: { apikey }
+					</span>
+					<span className="details-info-item">
+						PHP Version: { phpVersion }
+					</span>
+					<span className="details-info-item">
+						Post Types to Track: { postsToTrack }
+					</span>
+					<span className="details-info-item">
+						Page Types to Track: { pagesToTrack }
+					</span>
+					<span className="details-info-item">
+						Plugin Version: { pluginVersion }
+					</span>
+					<p onClick={ () => copyToClipboard() } className="copy-cta">Copy to Clipboard</p>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default SiteDetails;
