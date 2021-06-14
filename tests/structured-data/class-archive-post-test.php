deeed281fb2fcb1e558ab5abf9a284824f07aa0e
Gary Jones <gary.jones@automattic.com>
2021-06-15 00:08:46 +0100

    Tests: Remove duplicate homepage test

--- tests/structured-data/class-archive-post-test.php
+++ tests/structured-data/class-archive-post-test.php
@@ -23,58 +23,6 @@ final class Archive_Post_Test extends TestCase {
 	}
 
 	/**
-	 * Create 2 posts, set posts per page to 1, navigate to page 2 and test the structured data.
-	 *
-	 * @covers \Parsely::construct_parsely_metadata
-	 * @uses \Parsely::__construct
-	 * @uses \Parsely::get_author_name
-	 * @uses \Parsely::get_author_names
-	 * @uses \Parsely::get_bottom_level_term
-	 * @uses \Parsely::get_category_name
-	 * @uses \Parsely::get_clean_parsely_page_value
-	 * @uses \Parsely::get_coauthor_names
-	 * @uses \Parsely::get_current_url
-	 * @uses \Parsely::get_first_image
-	 * @uses \Parsely::get_options
-	 * @uses \Parsely::get_tags
-	 * @uses \Parsely::post_has_trackable_status
-	 * @uses \Parsely::update_metadata_endpoint
-	 * @group metadata
-	 */
-	public function test_home_page_for_posts_paged() {
-		// Setup Parsley object.
-		$parsely         = new \Parsely();
-		$parsely_options = get_option( \Parsely::OPTIONS_KEY );
-
-		// Insert 2 posts.
-		$page_id = self::factory()->post->create();
-		self::factory()->post->create();
-		$page = get_post( $page_id );
-
-		// Set permalinks, as Parsely currently strips ?page_id=... from the URL property.
-		// See https://github.com/Parsely/wp-parsely/issues/151
-		global $wp_rewrite;
-		$wp_rewrite->set_permalink_structure('/%postname%/');
-
-		// Set the homepage to show 1 post per page.
-		update_option( 'posts_per_page', 1 );
-
-		// Go to Page 2 of posts.
-		$this->go_to( home_url('/page/2' ) );
-
-		// Create the structured data for that post.
-		$structured_data = $parsely->construct_parsely_metadata( $parsely_options, $page );
-
-		// Check the required properties exist.
-		$this->assert_data_has_required_properties( $structured_data );
-
-		// The headline should be the name of the site, not the post_title of the latest post.
-		self::assertEquals( 'Test Blog', $structured_data['headline'] );
-		// The URL should be the current page, not the home url.
-		self::assertEquals( home_url('/page/2'), $structured_data['url'] );
-	}
-
-	/**
 	 * Check metadata for term archive.
 	 *
 	 * @covers \Parsely::construct_parsely_metadata
