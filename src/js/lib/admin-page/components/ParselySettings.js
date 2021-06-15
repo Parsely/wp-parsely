b16a13d3e5e2908c367b9298e13598ab4f362c02
jase-d-ace <andrada.jasonp@gmail.com>
2021-06-15 12:29:36 -0400

    rename file and wrap strings in i18n function

--- src/js/lib/admin-page/components/ParselySettings.js
+++ src/js/lib/admin-page/components/ParselySettings.js
@@ -2,7 +2,8 @@
  * External dependencies
  */
 import { useEffect, useState } from '@wordpress/element';
-import { Notice } from '@wordpress/components';
+import { Notice, SnackbarList } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -13,7 +14,7 @@ import SiteDetails from './SiteDetails';
 import WipeMetadataCache from './WipeMetadataCache';
 import WipeMetadataModal from './WipeMetadataModal';
 
-const App = () => {
+const ParselySettings = () => {
 	const [ settings, setSettings ] = useState( null );
 	const [ currentTab, setCurrentTab ] = useState( 'general' );
 	const [ loaded, setLoaded ] = useState( false );
@@ -30,7 +31,7 @@ const App = () => {
 			( errorFromServer ) => {
 				console.error( errorFromServer );
 				setNotice( {
-					content: 'Could not read settings from the site.',
+					content: __( 'Could not read settings from the site.', 'parsely-wp' ),
 					isDismissible: false,
 					status: 'error',
 				} );
@@ -58,11 +59,11 @@ const App = () => {
 		try {
 			const settingsFromServer = await saveSettingsToServer( settings );
 			setSettings( settingsFromServer );
-			setNotice( { content: 'Settings Successfully Updated!' } );
+			setNotice( { content: __( 'Settings Successfully Updated!', 'wp-parsely' ), status: 'success' } );
 			setCurrentTab( 'general' );
 		} catch ( writeError ) {
 			setNotice( {
-				content: 'Something went wrong! Please review your settings and try again.',
+				content: __( 'Something went wrong! Please review your settings and try again.', 'wp-parsely' ),
 				status: 'error',
 			} );
 			console.error( writeError );
@@ -80,13 +81,13 @@ const App = () => {
 	const nav = (
 		<nav className="controls">
 			<div className="nav-control" onClick={ () => _setCurrentTab( 'general' ) }>
-				<span className={ `${ currentTab === 'general' ? 'active' : '' }` }>General</span>
+				<span className={ `${ currentTab === 'general' ? 'active' : '' }` }>{ __( 'General', 'wp-parsely' ) }</span>
 			</div>
 			<div className="nav-control" onClick={ () => _setCurrentTab( 'advanced' ) }>
-				<span className={ `${ currentTab === 'advanced' ? 'active' : '' }` }>Advanced</span>
+				<span className={ `${ currentTab === 'advanced' ? 'active' : '' }` }>{ __( 'Advanced', 'wp-parsely' ) }</span>
 			</div>
 			<div className="nav-control" onClick={ () => _setCurrentTab( 'debug' ) }>
-				<span className={ `${ currentTab === 'debug' ? 'active' : '' }` }>Debug</span>
+				<span className={ `${ currentTab === 'debug' ? 'active' : '' }` }>{ __( 'Debug', 'wp-parsely' ) }</span>
 			</div>
 		</nav>
 	);
@@ -99,30 +100,30 @@ const App = () => {
 						disabled={ updatingSettings }
 						name="apikey"
 						value={ settings.apikey }
-						label="Site ID"
+						label={ __( 'Site ID', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Your SiteID is your own site domain"
+						note={ __( 'Your SiteID is your own site domain', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="apiSecret"
 						value={ 'no secret given' }
-						label="API Secret"
+						label={ __( 'API Secret', 'wp-parsely' ) }
 						onChange={ handleInputChange }
 						note="Your API Secret is your secret code to access our API"
 					/>
 					<Setting
 						name="logo"
 						value={ settings.logo }
-						label="Logo"
+						label={ __( 'Logo', 'wp-parsely') }
 						onChange={ handleInputChange }
-						note="You can pass a URL to set your site's logo"
+						note={ __( 'You can pass a URL to set your site\'s logo', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="track_post_types"
 						value={ settings.track_post_types }
-						label="Track Post Types"
+						label={ __( 'Track Post Types', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="...."
+						note={ __( '....', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="track_page_types"
@@ -136,72 +137,72 @@ const App = () => {
 					<Setting
 						name="meta_type"
 						value={ settings.meta_type }
-						label="Metadata Type"
+						label={ __( 'Metadata Type', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Choose the metadata format for us to track"
+						note={ __( 'Choose the metadata format for us to track', 'wp-parsely')}
 					/>
 					<Setting
 						name="custom_taxonomy_section"
 						value={ settings.custom_taxonomy_section }
-						label="Custom Taxonomy Section"
+						label={ __( 'Custom Taxonomy Section', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: Category. Choose the default taxonomy to map to Parse.ly sections"
+						note={ __('Default: Category. Choose the default taxonomy to map to Parse.ly sections', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="content_id_prefix"
 						value={ settings.content_id_prefix }
-						label="Content ID Prefix"
+						label={ __( 'Content ID Prefix', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Choose a custom prefix for your content"
+						note={ __( 'Choose a custom prefix for your content', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="disable_javascript"
 						value={ settings.disable_javascript }
-						label="Disable Javascript"
+						label={ __( 'Disable Javascript', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: Off. Disable our javascript tracking if you use a separate system for JS tracking"
+						note={ __( 'Default: Off. Disable our javascript tracking if you use a separate system for JS tracking', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="disable_amp"
 						value={ settings.disable_amp }
-						label="Disable AMP"
+						label={ __( 'Disable AMP', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: On. Disable our AMP tracking if you use a separate system to track AMP content"
+						note={ __( 'Default: On. Disable our AMP tracking if you use a separate system to track AMP content', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="use_top_level_cats"
 						value={ settings.use_top_level_cats }
-						label="Use Top-Level Categories"
+						label={ __( 'Use Top-Level Categories', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: On. Choose if you want the first top-level category to be mapped to Parse.ly"
+						note={ __( 'Default: On. Choose if you want the first top-level category to be mapped to Parse.ly', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="cats_as_tags"
 						value={ settings.cats_as_tags }
-						label="Categories as Tags"
+						label={ __( 'Categories as Tags', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: On. Choose if you want your non-primary categories to appear as tags"
+						note={ __( 'Default: On. Choose if you want your non-primary categories to appear as tags', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="track_authenticated_users"
 						value={ settings.track_authenticated_users }
-						label="Track Authenticated Users"
+						label={ __( 'Track Authenticated Users', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: On."
+						note={ __( 'Default: On.', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="lowercase_tags"
 						value={ settings.lowercase_tags }
-						label="Lowercase Tags"
+						label={ __( 'Lowercase Tags', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: On. Choose if you want your tags to be converted to lower case"
+						note={ __( 'Default: On. Choose if you want your tags to be converted to lower case', 'wp-parsely' ) }
 					/>
 					<Setting
 						name="force_https_canonicals"
 						value={ settings.force_https_canonicals }
-						label="Force HTTPS Canonical URLs"
+						label={ __( 'Force HTTPS Canonical URLs', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="Default: Off. Choose if you want your canonicals to use the HTTPS scheme"
+						note={ __( 'Default: Off. Choose if you want your canonicals to use the HTTPS scheme', 'wp-parsely' ) }
 					/>
 				</div>
 				<div className={ `tab-body debug ${ displayDiv( 'debug', currentTab ) }` }>
@@ -218,22 +219,22 @@ const App = () => {
 					<Setting
 						name="metadata_secret"
 						value={ settings.metadata_secret }
-						label="Metadata Secret"
+						label={ __( 'Metadata Secret', 'wp-parsely' ) }
 						onChange={ handleInputChange }
-						note="The metadata secret provided to you by Parse.ly"
+						note={ __( 'The metadata secret provided to you by Parse.ly', 'wp-parsely' ) }
 					/>
 					<WipeMetadataCache
 						setting={ { parsely_wipe_metadata_cache: settings.parsely_wipe_metadata_cache } }
-						label="Wipe Metadata Cache"
+						label={ __( 'Wipe Metadata Cache', 'wp-parsely' ) }
 						onClick={ showModal }
-						note="This will wipe all of your site's metadata and resend all metadata to Parse.ly"
+						note={ __( 'This will wipe all of your site\'s metadata and resend all metadata to Parse.ly', 'wp-parsely' ) }
 					/>
 					<SiteDetails
 						apikey={ settings.apikey }
 						postsToTrack={ settings.track_post_types }
 						pagesToTrack={ settings.track_page_types }
-						pluginVersion="2.5"
-						phpVersion="7.4.1"
+						pluginVersion={ __('2.5', 'wp-parsely' ) }
+						phpVersion={ __( '7.4.1', 'wp-parsely' ) }
 					/>
 				</div>
 			</div>
@@ -241,7 +242,7 @@ const App = () => {
 				disabled={ updatingSettings }
 				type="submit"
 				className="button-primary"
-				value="do the thing!"
+				value={ __( 'do the thing!', 'wp-parsely' ) }
 			/>
 		</form>
 	);
@@ -250,6 +251,7 @@ const App = () => {
 
 	return (
 		<div className="settings-container">
+
 			{ ! loaded && isError && ! notice.isDismissible && (
 				<Notice status="error" isDismissible={ false }>
 					{ notice.content }
@@ -257,10 +259,7 @@ const App = () => {
 			) }
 			{ ! loaded && ! isError && <h2>Loading...</h2> }
 			{ loaded && notice && ! isError && (
-				<Notice
-					onRemove={ removeNotice }
-					status="success"
-				>
+				<Notice onRemove={ removeNotice } status="success">
 					{ notice.content }
 				</Notice>
 			) }
@@ -270,4 +269,4 @@ const App = () => {
 	);
 };
 
-export default App;
+export default ParselySettings;
