361a1b59577ec548f29fcd771659dbe0b5bd434b
Jeff Bowen <jblz@users.noreply.github.com>
2021-07-09 13:59:31 -0400

    pull block entry code out of function

--- src/blocks/recommendations/index.php
+++ src/blocks/recommendations/index.php
@@ -5,23 +5,13 @@
  * @package Parsely
  */
 
-/**
- * Wrapper function to kick off the plugin. Sources & hooks initialization functions for the block & API classes.
- *
- * @return void
- */
-function wp_parsely_recommendations_block_init() {
-	global $wp_version;
-
-	if ( version_compare( $wp_version, '5.6' ) < 0 ) {
-		// WordPress is not recent enough to run this block.
-		return;
-	}
-
-	require __DIR__ . '/class-parsely-recommendations-block.php';
-	require __DIR__ . '/class-parsely-recommendations-block-api.php';
-
-	add_action( 'init', 'Parsely_Recommendations_Block::register_block_and_assets' );
-	add_action( 'rest_api_init', array( 'Parsely_Recommendations_Block_API', 'rest_api_init' ) );
+if ( version_compare( $wp_version, '5.6' ) < 0 ) {
+	// WordPress is not recent enough to run this block.
+	return;
 }
-wp_parsely_recommendations_block_init();
+
+require __DIR__ . '/class-parsely-recommendations-block.php';
+require __DIR__ . '/class-parsely-recommendations-block-api.php';
+
+add_action( 'init', 'Parsely_Recommendations_Block::register_block_and_assets' );
+add_action( 'rest_api_init', array( 'Parsely_Recommendations_Block_API', 'rest_api_init' ) );
