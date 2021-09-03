099181f9fb7213dc4c7a56da948790f056c71ff0
Jeff Bowen <jblz@users.noreply.github.com>
2021-09-03 10:26:37 -0400

    update build

--- build/recommended-widget.asset.php
+++ build/recommended-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '856ea10160d45dfa92ae341c4560d4b6');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '5d5c314dd64559697b1e1370dab71b55');
