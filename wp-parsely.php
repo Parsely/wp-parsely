afa94696fec1c1d6e7758c8b3806b1b8e9dda401
Pau Argelaguet <pau@outlook.com>
2021-09-16 13:02:37 +0200

    Renaming alpha

--- wp-parsely.php
+++ wp-parsely.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Parse.ly
  * Plugin URI:        https://www.parse.ly/help/integration/wordpress
  * Description:       This plugin makes it a snap to add Parse.ly tracking code to your WordPress blog.
- * Version:           2.5.2
+ * Version:           2.6.0-alpha
  * Author:            Parse.ly
  * Author URI:        https://www.parse.ly
  * Text Domain:       wp-parsely
@@ -31,7 +31,7 @@ if ( class_exists( 'Parsely' ) ) {
 	return;
 }
 
-define( 'PARSELY_VERSION', '2.5.2' );
+define( 'PARSELY_VERSION', '2.6.0-alpha' );
 
 if ( ! defined( 'PARSELY_PLUGIN_BASENAME' ) ) {
 	define( 'PARSELY_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
