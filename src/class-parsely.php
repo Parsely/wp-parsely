b97486df862a6b8d98e47f6438f9edcb7382ba42
Pau Argelaguet <pauarge@users.noreply.github.com>
2021-11-02 11:16:33 +0100

    Adding filter to display Cloudflare links (#473)
    * Adding filter to display CloudFlare links
    
    * Adding test for filter
    
    * Updating failing test
    
    * Fix multisite test
    
    * Updating readme
    
    * Update README.md
    
    Co-authored-by: Gary Jones <gary.jones@automattic.com>
    
    * Update README.md
    
    Co-authored-by: Gary Jones <gary.jones@automattic.com>
    
    * Update src/class-parsely.php
    
    Co-authored-by: Gary Jones <gary.jones@automattic.com>
    
    * Updating filter docs
    
    * Removing redundant test assertions
    
    * Changing name of the filter
    
    * Update README.md
    
    Co-authored-by: Gary Jones <gary.jones@automattic.com>
    
    Co-authored-by: Gary Jones <gary.jones@automattic.com>

--- src/class-parsely.php
+++ src/class-parsely.php
@@ -786,9 +786,19 @@ class Parsely {
 				'wp-parsely-recommended-widget',
 			)
 		) ) {
-			// Have CloudFlare Rocket Loader ignore these scripts:
-			// https://support.cloudflare.com/hc/en-us/articles/200169436-How-can-I-have-Rocket-Loader-ignore-specific-JavaScripts-.
-			$tag = preg_replace( '/^<script /', '<script data-cfasync="false" ', $tag );
+			/**
+			 * Filter whether to include the CloudFlare Rocket Loader attribute (`data-cfasync=false`) in the script.
+			 * Only needed if the site being served is behind Cloudflare. Should return false otherwise.
+			 * https://support.cloudflare.com/hc/en-us/articles/200169436-How-can-I-have-Rocket-Loader-ignore-specific-JavaScripts
+			 *
+			 * @since 3.0.0
+			 *
+			 * @param bool $enabled True if enabled, false if not.
+			 * @param string $handle The script's registered handle.
+			 */
+			if ( apply_filters( 'wp_parsely_enable_cfasync_attribute', false, $handle ) ) {
+				$tag = preg_replace( '/^<script /', '<script data-cfasync="false" ', $tag );
+			}
 		}
 
 		if ( 'wp-parsely-tracker' === $handle ) {
