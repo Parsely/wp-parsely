e6a4d921b656b3a579a7b5d4c4d6710571e1abed
Pau Argelaguet <pau@outlook.com>
2021-11-12 12:08:43 +0100

    Adding testing class

---
+++ tests/Integration/RestTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Parsely REST API tests.
+ *
+ * @package Parsely\Tests
+ */
+
+declare(strict_types=1);
+
+namespace Parsely\Tests\Integration;
+
+use Parsely\Parsely;
+use Parsely\Rest;
+
+
+/**
+ * Parsely REST API tests.
+ */
+final class RestTest extends TestCase {
+	/**
+	 * Internal variable
+	 *
+	 * @var Rest $rest Holds the Rest object
+	 */
+	private static $rest;
+
+	/**
+	 * The setUp run before each test
+	 */
+	public function set_up(): void {
+		parent::set_up();
+
+		self::$rest = new Rest( new Parsely() );
+	}
+}
