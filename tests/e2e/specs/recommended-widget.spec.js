fc2a57be3d87c842683b2b04dac790f8ebfbc12f
Pau Argelaguet <pau@outlook.com>
2021-11-17 11:38:07 +0100

    Shorter timeout for tests

--- tests/e2e/specs/recommended-widget.spec.js
+++ tests/e2e/specs/recommended-widget.spec.js
@@ -52,7 +52,6 @@ const checkForNonActiveWidgetText = async () => {
 };
 
 describe( 'Recommended widget', () => {
-	jest.setTimeout( 30000 );
 
 	beforeAll( () => {
 		page.once( 'dialog', async function( dialog ) {
