1f951254aa0696b24023f851d5f92693953887de
Pau Argelaguet <pau@outlook.com>
2021-11-26 10:21:05 +0100

    Fixing the coauthors integration

--- src/class-parsely.php
+++ src/class-parsely.php
@@ -861,6 +861,10 @@ class Parsely {
 		 */
 		$authors = apply_filters( 'wp_parsely_pre_authors', $authors, $post );
 
+		// Filtering falsy values from the array
+		$authors = array_filter($authors, function($x) { return !(is_null($x) || $x === false); });
+
+		// Getting the author name for each author
 		$authors = array_map( array( $this, 'get_author_name' ), $authors );
 
 		/**
