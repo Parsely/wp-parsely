131255a891296f61e4423c53e1798620059bd372
Pau Argelaguet <pau@outlook.com>
2021-12-01 10:02:07 +0100

    Adding object type filter

--- src/class-rest.php
+++ src/class-rest.php
@@ -79,8 +79,17 @@ class Rest {
 
 		$options     = $this->parsely->get_options();
 		$object_type = array_unique( array_merge( $options['track_post_types'], $options['track_page_types'] ) );
-		$args        = array( 'get_callback' => $callback );
 
+		/**
+		 * Filters the list of author object types that the Parse.ly REST API is hooked into.
+		 *
+		 * @since 3.1.0
+		 *
+		 * @param string[] $object_type Array of strings containing the object types, i.e. `page`, `post`, `term`.
+		 */
+		$object_type = apply_filters( 'wp_parsely_rest_object_type', $object_type );
+
+		$args = array( 'get_callback' => $callback );
 		register_rest_field( $object_type, self::PARSELY_REST_FIELD_NAME, $args );
 	}
 }
