bd32515fe51f55b8509db314d6b8c11c92ff9b47
Jeff Bowen <jblz@users.noreply.github.com>
2022-01-25 14:33:35 -0500

    Customize the links on the Network Plugin List when network-active

--- src/UI/class-plugins-actions.php
+++ src/UI/class-plugins-actions.php
@@ -39,6 +39,19 @@ final class Plugins_Actions {
 	 * @return array
 	 */
 	public function add_plugin_meta_links( array $actions ): array {
+		if ( ( is_multisite() && is_plugin_active_for_network( plugin_basename( PARSELY_FILE ) ) ) ) {
+			$actions['siteslist']     = sprintf(
+				'<a href="%s" target="_blank">%s</a>',
+				network_admin_url( 'sites.php' ),
+				esc_html__( 'Sites', 'wp-parsely' )
+			);
+			$actions['documentation'] = sprintf(
+				'<a href="%s" target="_blank">%s</a>',
+				esc_url( 'https://www.parse.ly/help/integration/wordpress' ),
+				esc_html__( 'Documentation', 'wp-parsely' )
+			);
+		}
+
 		$settings_link = sprintf(
 			'<a href="%s">%s</a>',
 			esc_url( Parsely::get_settings_url() ),
