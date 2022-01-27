f005d9ee4cebf75f0c34636f5e531dada02eff36
Jeff Bowen <jblz@users.noreply.github.com>
2022-01-27 17:49:11 -0500

    Show an informational notice on the settings page for the main site of a multisite install

--- views/parsely-settings.php
+++ views/parsely-settings.php
@@ -20,6 +20,15 @@ $parsely_version_string = sprintf( __( 'Version %s', 'wp-parsely' ), Parsely::VE
 <div class="wrap">
 	<h1 class="wp-heading-inline"><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<span id="wp-parsely_version"><?php echo esc_html( $parsely_version_string ); ?></span>
+<?php
+if ( is_multisite() && is_main_site() ) {
+	?>
+		<div class="notice notice-info">
+			<p><?php esc_html_e( 'Attention: this is the main site of your Multisite Network.', 'wp-parsely' ); ?></p>
+		</div>
+	<?php
+}
+?>
 	<form name="parsely" method="post" action="options.php" novalidate>
 		<?php
 		settings_fields( Parsely::OPTIONS_KEY );
