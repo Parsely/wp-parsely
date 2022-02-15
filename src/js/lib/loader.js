25584df79ece19211355a6e327c7e60e2144300f
Pau Argelaguet <pau@outlook.com>
2022-02-15 11:41:17 +0100

    Adding documentation to JS hook

--- src/js/lib/loader.js
+++ src/js/lib/loader.js
@@ -3,6 +3,11 @@ import { createHooks } from '@wordpress/hooks';
 window.wpParselyHooks = createHooks();
 
 export function wpParselyInitCustom() {
+	/**
+	 * The `wpParselyOnLoad` hook gets called with the `onLoad` event of the `window.PARSELY` object.
+	 * All functions enqueued on that hook will be executed on that event according to their priorities. Those
+	 * functions should not expect any parameters and shouldn't return any.
+	 */
 	const customOnLoad = () => window.wpParselyHooks.doAction( 'wpParselyOnLoad' );
 
 	if ( typeof window.PARSELY === 'object' ) {
