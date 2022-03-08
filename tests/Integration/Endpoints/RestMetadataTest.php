74c4eda3fc127ce1caddd1fe3ad11b408c5cb359
Pau Argelaguet <pauarge@users.noreply.github.com>
2022-03-08 18:16:07 +0100

    Update tests/Integration/Endpoints/RestMetadataTest.php
    Co-authored-by: Alex Cicovic <23142906+acicovic@users.noreply.github.com>

--- tests/Integration/Endpoints/RestMetadataTest.php
+++ tests/Integration/Endpoints/RestMetadataTest.php
@@ -76,7 +76,7 @@ final class RestMetadataTest extends TestCase {
 	}
 
 	/**
-	 * Test whether the logic has been enqueued when the `run` method is called with no API key.
+	 * Verify that the logic has not been enqueued when the `run` method is called with no API key.
 	 *
 	 * @covers \Parsely\Endpoints\Rest_Metadata::run
 	 */
