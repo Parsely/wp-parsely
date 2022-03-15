ea95590fe155793b4af0c9c2b54c04ed4aae69ea
Pau Argelaguet <pau@outlook.com>
2022-03-15 14:58:42 +0100

    Providing a Store

--- tests/blocks/recommendations/block.test.js
+++ tests/blocks/recommendations/block.test.js
@@ -7,9 +7,14 @@ import { render } from '@testing-library/react';
  * Internal dependencies
  */
 import ParselyRecommendations from '../../../src/blocks/recommendations/components/parsely-recommendations';
+import RecommendationsStore from '../../../src/blocks/recommendations/recommendations-store';
 
 test( 'My sample test', () => {
-	render( <ParselyRecommendations /> );
+	render(
+		<RecommendationsStore>
+			<ParselyRecommendations />
+		</RecommendationsStore>
+	);
 
 	expect( true ).toBeTruthy();
 } );
