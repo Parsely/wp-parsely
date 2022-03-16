db5a51079e8010d1d6dd03ffede9c35e22625410
Gary Jones <gary.jones@automattic.com>
2022-03-16 22:54:45 +0000

    Switch messy integration test to unit test
    Instead of stubbing out WPGraphQL functions under a `Parsely\Endpoints` namespace, and having it set public properties in the test class, let's use BrainMonkey to simply patch the missing external functions instead.
    
    This commit adds a new unit test that includes a test for whether these functions defined in the WPGraphQL plugin are called. We use `Brain\Monkey\Functions\expect()` to name the functions we're expecting to be called, and then also state how many times we expect them to be called, and also what arguments (values or type) they should be called with. If in the course of running the test, anything differs, the test fails. Otherwise, it passes.
    
    The `Brain\Monkey\Functions\when()` call is there to mock the WordPress-defined `__()` function, since in a unit test, WordPress isn't loaded. In this case, we don't care what it was called with, but we patch it to simple return the first argument that was passed to it (the string to be translated).

--- tests/Integration/Endpoints/GraphQLMetadataTest.php
+++ tests/Integration/Endpoints/GraphQLMetadataTest.php
@@ -18,16 +18,6 @@ use Parsely\Tests\Integration\TestCase;
  */
 final class GraphQLMetadataTest extends TestCase {
 	/**
-	 * Stores
-	 *
-	 * @var array<string, mixed> $call_history
-	 */
-	public static $call_history = array(
-		'register_graphql_object_type' => false,
-		'register_graphql_field'       => false,
-	);
-
-	/**
 	 * Internal variable
 	 *
 	 * @var GraphQL_Metadata $graphql Holds the GraphQL object
@@ -91,66 +81,4 @@ final class GraphQLMetadataTest extends TestCase {
 		self::$graphql->run();
 		self::assertFalse( has_filter( 'graphql_register_types', array( self::$graphql, 'register_meta' ) ) );
 	}
-
-	/**
-	 * Tests if the functions to register items into GraphQL have been called.
-	 *
-	 * @since 3.2.0
-	 *
-	 * @covers \Parsely\Endpoints\GraphQL_Metadata::register_meta
-	 * @uses \Parsely\Endpoints\GraphQL_Metadata::register_object_types()
-	 * @uses \Parsely\Endpoints\GraphQL_Metadata::register_fields()
-	 */
-	public function test_graphql_registers_types(): void {
-		self::$graphql->register_meta();
-
-		self::assertEquals( 'ParselyMetaContainer', self::$call_history['register_graphql_object_type']['type_name'] );
-		self::assertEquals( 'ContentNode', self::$call_history['register_graphql_field']['type_name'] );
-		self::assertEquals( 'parsely', self::$call_history['register_graphql_field']['field_name'] );
-
-		self::$call_history['register_graphql_object_type'] = false;
-		self::$call_history['register_graphql_field']       = false;
-	}
-}
-
-
-/**
- * Mock WPGraphQL functions. The plugin is not installed by default with wp-parsely.
- */
-namespace Parsely\Endpoints;
-
-/**
- * Mock function.
- *
- * @see https://wpgraphqldocs.gatsbyjs.io/functions/register_graphql_object_type/
- *
- * @param string $type_name The name of the Type. This must be unique across all Types of any kind in the GraphQL Schema.
- * @param array  $config Configuration for the field.
- *
- * @return void
- */
-function register_graphql_object_type( string $type_name, array $config ) {
-	\Parsely\Tests\Integration\Endpoints\GraphQLMetadataTest::$call_history['register_graphql_object_type'] = array(
-		'type_name' => $type_name,
-		'config'    => $config,
-	);
-}
-
-/**
- * Mock function.
- *
- * @see https://wpgraphqldocs.gatsbyjs.io/functions/register_graphql_field/
- *
- * @param string $type_name The name of the GraphQL Type in the Schema to register the field to.
- * @param string $field_name The name of the field. Should be unique to the Type the field is being registered to.
- * @param array  $config Configuration for the field.
- *
- * @return void
- */
-function register_graphql_field( string $type_name, string $field_name, array $config ): void {
-	\Parsely\Tests\Integration\Endpoints\GraphQLMetadataTest::$call_history['register_graphql_field'] = array(
-		'type_name'  => $type_name,
-		'field_name' => $field_name,
-		'config'     => $config,
-	);
 }
