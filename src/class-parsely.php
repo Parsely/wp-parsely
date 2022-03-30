fc03bedd794aacdf412cf405dadaa31f1a8dbd6d
Pau Argelaguet <pau@outlook.com>
2022-03-30 14:38:41 +0200

    Remove hard deprecate

--- src/class-parsely.php
+++ src/class-parsely.php
@@ -343,7 +343,6 @@ class Parsely {
 	 * @return array<string, mixed>
 	 */
 	public function construct_parsely_metadata( array $parsely_options, WP_Post $post ): array {
-		_deprecated_function( 'Parsely::construct_parsely_metadata', '3.3.0' );
 		$metadata = new Metadata( $this );
 		return $metadata->construct_metadata( $parsely_options, $post );
 	}
