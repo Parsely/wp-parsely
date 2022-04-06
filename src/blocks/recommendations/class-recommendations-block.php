42e3191d4343a2b877f096dec121bfe2fcfe1231
Ryan Welcher <ryan.welcher@automattic.com>
2022-04-06 11:44:04 -0400

    Remove the manually registered scripts and styles in favor of the block.json managing them. Manually enqueue the viewScript defined in block.json.

--- src/blocks/recommendations/class-recommendations-block.php
+++ src/blocks/recommendations/class-recommendations-block.php
@@ -38,7 +38,7 @@ class Recommendations_Block {
 			return;
 		}
 
-		self::register_block_and_assets();
+		self::register_block();
 	}
 
 	/**
@@ -49,34 +49,7 @@ class Recommendations_Block {
 	 *
 	 * @return void
 	 */
-	public static function register_block_and_assets(): void {
-		$plugin_url = plugin_dir_url( PARSELY_FILE );
-
-		$editor_asset_file = require plugin_dir_path( PARSELY_FILE ) . 'build/recommendations-edit.asset.php';
-		wp_register_script(
-			'wp-parsely-recommendations-block-editor',
-			$plugin_url . 'build/recommendations-edit.js',
-			$editor_asset_file['dependencies'],
-			$editor_asset_file['version'],
-			true
-		);
-
-		$script_asset_file = require plugin_dir_path( PARSELY_FILE ) . 'build/recommendations.asset.php';
-		wp_register_script(
-			'wp-parsely-recommendations-block',
-			$plugin_url . 'build/recommendations.js',
-			$script_asset_file['dependencies'],
-			$script_asset_file['version'],
-			true
-		);
-
-		wp_register_style(
-			'wp-parsely-recommendations-block',
-			$plugin_url . 'build/style-recommendations-edit.css',
-			array(),
-			$script_asset_file['version']
-		);
-
+	public static function register_block(): void {
 		/**
 		 * Register the block by passing the path to it's block.json file that contains the majority of it's definition.
 		 * This file will be copied into `build/blocks/recommendations` by the build process and should be accessed there.
@@ -99,10 +72,7 @@ class Recommendations_Block {
 	 */
 	private static function get_block_registration_args(): array {
 		return array(
-			'editor_script'   => 'wp-parsely-recommendations-block-editor',
 			'render_callback' => __CLASS__ . '::render_callback',
-			'script'          => 'wp-parsely-recommendations-block',
-			'style'           => 'wp-parsely-recommendations-block',
 		);
 	}
 
@@ -115,6 +85,13 @@ class Recommendations_Block {
 	 * @return string
 	 */
 	public static function render_callback( array $attributes ): string {
+		/**
+		 * In block.json we define a `viewScript` that is mean to only be loaded on the front end.
+		 * We need to manually enqueue this script here.
+		 *
+		 * The slug is automatically generated as {namespace}-{block-name}-view-script
+		 */
+		wp_enqueue_script( 'wp-parsely-recommendations-view-script' );
 		ob_start();
 		?>
 <section
