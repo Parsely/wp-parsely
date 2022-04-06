ae749abc2a74ce6b3b4b82e343530b898e2458f4
Ryan Welcher <ryan.welcher@automattic.com>
2022-04-06 11:49:50 -0400

    Add the build files for the block.

---
+++ build/blocks/recommendations/edit.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'aacc15c621cf996a59dd2c6dbd7ec2e1');
