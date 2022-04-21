415eea416aad66c942525c94238906b2291b68d6
Pau Argelaguet <pau@outlook.com>
2022-04-21 11:11:11 +0200

    Updating asset

--- build/loader.asset.php
+++ build/loader.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks', 'wp-polyfill'), 'version' => 'b1936f32bf22bf6bffa0485693455e4c');
+<?php return array('dependencies' => array('wp-hooks', 'wp-polyfill'), 'version' => 'd27cf7184cf7bd8691adc3c17aedc470');
