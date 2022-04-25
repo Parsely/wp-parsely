0239e1f57c7d008c8c572395a9e0f11b550d8f0b
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-04-25 12:23:20 +0300

    Rebuild assets

--- build/blocks/recommendations/edit.asset.php
+++ build/blocks/recommendations/edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '91e20fabcb577bff112b256826552027');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '47dadfc076696aceae5ed02c88f090d1');
