f1dc6f74d8b997652a397c9299a3027389e63834
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-04-25 10:46:56 +0300

    Recommendations Block: Handle more failed HTTP error scenarios

--- build/blocks/recommendations/view.asset.php
+++ build/blocks/recommendations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '95b1eacab15e955cdbec26cdd2d22fc3');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'a62e512585ed358be80085aa5079d346');
--- src/blocks/recommendations/components/parsely-recommendations.js
+++ src/blocks/recommendations/components/parsely-recommendations.js
@@ -27,13 +27,15 @@ export default function ParselyRecommendations( {
 
 	function getErrorMessage() {
 		let errorMessage = `${ __( 'Error:', 'wp-parsely' ) } ${ JSON.stringify( error ) }`;
+		const httpError = errorMessage.includes( '"errors":{"http_request_failed"' ) ||
+		( typeof error === 'object' && error?.code === 'fetch_error' );
 
-		if ( errorMessage.includes( '{"errors":{"403":["Forbidden"]},"error_data":[]}' ) ) {
+		if ( httpError ) {
+			errorMessage = __( 'The Parse.ly Recommendations API is not accessible. You may be offline.', 'wp-parsely' );
+		} else if ( errorMessage.includes( '{"errors":{"403":["Forbidden"]},"error_data":[]}' ) ) {
 			errorMessage = __( 'Access denied. Please verify that your Site ID is valid.', 'wp-parsely' );
 		} else if ( typeof error === 'object' && error?.code === 'rest_no_route' ) {
 			errorMessage = __( 'The REST route is unavailable. To use it, wp_parsely_enable_related_api_proxy should be true.', 'wp-parsely' );
-		} else if ( errorMessage.includes( '{"errors":{"http_request_failed":["A valid URL was not provided."]},"error_data":[]}' ) ) {
-			errorMessage = __( 'The Parse.ly Recommendations API is not accessible. You may be offline.', 'wp-parsely' );
 		}
 
 		return errorMessage;
