0ef5a5c9bca06f842b3a6ca0f98200520dd71907
Pau Argelaguet <pauarge@users.noreply.github.com>
2022-04-26 10:44:51 +0200

    Adding onReady hook (#808)
    Co-authored-by: Alex Cicovic <23142906+acicovic@users.noreply.github.com>

--- src/js/lib/loader.js
+++ src/js/lib/loader.js
@@ -4,11 +4,18 @@ window.wpParselyHooks = createHooks();
 
 export function wpParselyInitCustom() {
 	/**
-	 * The `wpParselyOnLoad` hook gets called with the `onLoad` event of the `window.PARSELY` object.
+	 * The `wpParselyOnLoad` hook gets called with the `onload` event of the `window.PARSELY` object.
 	 * All functions enqueued on that hook will be executed on that event according to their priorities. Those
 	 * functions should not expect any parameters and shouldn't return any.
 	 */
 	const customOnLoad = () => window.wpParselyHooks.doAction( 'wpParselyOnLoad' );
+
+	/**
+	 * The `wpParselyOnReady` hook gets called with the `onReady` event of the `window.PARSELY` object.
+	 * All functions enqueued on that hook will be executed on that event according to their priorities. Those
+	 * functions should not expect any parameters and shouldn't return any.
+	 */
+	const customOnReady = () => window.wpParselyHooks.doAction( 'wpParselyOnReady' );
 
 	// Construct window.PARSELY object.
 	if ( typeof window.PARSELY === 'object' ) {
@@ -23,9 +30,22 @@ export function wpParselyInitCustom() {
 				customOnLoad();
 			};
 		}
+
+		if ( typeof window.PARSELY.onReady !== 'function' ) {
+			window.PARSELY.onReady = customOnReady;
+		} else {
+			const oldOnReady = window.PARSELY.onReady;
+			window.PARSELY.onReady = function() {
+				if ( oldOnReady ) {
+					oldOnReady();
+				}
+				customOnReady();
+			};
+		}
 	} else {
 		window.PARSELY = {
 			onload: customOnLoad,
+			onReady: customOnReady,
 		};
 	}
 
