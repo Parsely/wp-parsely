40e177b5d9c895e1e7675ca04891c41e8a3a449d
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-05-13 12:19:16 +0300

    Update plugin version number to 3.2.2

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,6 +1,6 @@
 import { activatePlugin, loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '3.3.1';
+export const PLUGIN_VERSION = '3.3.2';
 
 export const waitForWpAdmin = () => page.waitForSelector( 'body.wp-admin' );
 
