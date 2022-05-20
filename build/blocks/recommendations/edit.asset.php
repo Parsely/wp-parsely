41c3febf09f8ba66975ab1069baa72323b729c70
Pau Argelaguet <pau@outlook.com>
2022-05-20 11:29:32 +0200

    Removing TS files

--- build/blocks/recommendations/edit.asset.php
+++ build/blocks/recommendations/edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '541f3541b791d8e8c735');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '6dc89a8a93901a299ab9');
--- build/blocks/recommendations/view.asset.php
+++ build/blocks/recommendations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '91d59ba34ec9cbcc92f9');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'cc8f3e1e086ed43f320d');
