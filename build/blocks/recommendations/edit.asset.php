4fdcab9b65c7d80705dfac363f28e737d0b119ac
Pau Argelaguet <pauarge@users.noreply.github.com>
2022-05-24 10:49:01 +0200

    Fix logo on Recommendations Block (#901)
    * Fix Parse.ly logo on Recommendations Block
    
    * Fix Parse.ly logo on Recommendations Block

--- build/blocks/recommendations/edit.asset.php
+++ build/blocks/recommendations/edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f38a77ad8115a8a39d36');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3e3314fba6773e7b1934');
