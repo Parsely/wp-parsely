cb9c1c24e2a7ae4d41f4e1af309562f69ef1b296
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-05-31 16:18:22 +0300

    ContentHelperProvider: Add built files

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-url'), 'version' => '1c8a7d5c11e0ac5bce08');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-url'), 'version' => 'de250f1e8ee59be61ab5');
