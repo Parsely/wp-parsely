4704f34c2727f9c4ae7c3a6e265949b8f498ec48
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-07-13 11:33:57 +0300

    Content Helper: Restore sidebar class

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '1cea98222653366a76ec');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '5423fa419c50ca1fd509');
