d72c3eeb00fd65a1ed2290193ad1c7136a9ee065
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-09-12 17:52:46 +0300

    Revert "Content Helper: Fix broken links in "Post Stats" button"
    This reverts commit f6cb245d3af743d02e797862639123fc51f5c40c.

--- src/blocks/content-helper/class-content-helper.php
+++ src/blocks/content-helper/class-content-helper.php
@@ -18,16 +18,6 @@ namespace Parsely;
 class Content_Helper {
 
 	/**
-	 * Constructor.
-	 *
-	 * @param Parsely $parsely Parsely instance.
-	 * @since 3.5.1
-	 */
-	public function __construct( Parsely $parsely ) {
-		$this->parsely = $parsely;
-	}
-
-	/**
 	 * Inserts the Content Helper into the WordPress Post Editor.
 	 *
 	 * @since 3.5.0
@@ -49,12 +39,6 @@ class Content_Helper {
 			array(),
 			$content_helper_asset['version']
 		);
-
-		// Set wpParselyContentHelperPrefix for the "Post Stats" button links
-		// to point to the correct location.
-		$prefix                = trailingslashit( 'https://dash.parsely.com/' . esc_js( $this->parsely->get_api_key() ) ) . 'find';
-		$analytics_link_prefix = 'window.wpParselyContentHelperPrefix = "' . $prefix . '";';
-		wp_add_inline_script( 'wp-parsely-block-content-helper', $analytics_link_prefix, 'before' );
 	}
 
 }
