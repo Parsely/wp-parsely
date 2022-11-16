793977a29077b502c6a8e047ab30cbbfddf2c8a7
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-11-16 11:17:09 +0200

    Content Helper Current Post Details: Redesign for a more Parse.ly feel

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '3d8ac2e0f8aee53ec695');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '7edddea310d2d6088cda');
--- src/blocks/content-helper/current-post-details/component.tsx
+++ src/blocks/content-helper/current-post-details/component.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Spinner } from '@wordpress/components';
+import { Button, Spinner } from '@wordpress/components';
 import { useEffect, useState } from '@wordpress/element';
 
 /**
@@ -17,7 +17,7 @@ const FETCH_RETRIES = 3;
 /**
  * Specifies the form of component props.
  */
-interface PostDetailsTableProps {
+interface PostDetailsSectionProps {
 	data: PostPerformanceData;
 }
 
@@ -28,10 +28,11 @@ function CurrentPostDetails() {
 	const [ loading, setLoading ] = useState<boolean>( true );
 	const [ error, setError ] = useState( null );
 	const [ postDetailsData, setPostDetails ] = useState<PostPerformanceData>( null );
+	const provider = new CurrentPostDetailsProvider();
 
 	useEffect( () => {
 		const fetchPosts = async ( retries: number ) => {
-			CurrentPostDetailsProvider.getCurrentPostDetails()
+			provider.getCurrentPostDetails()
 				.then( ( result ) => {
 					setPostDetails( result );
 					setLoading( false );
@@ -69,98 +70,123 @@ function CurrentPostDetails() {
 
 	return (
 		<>
-			{ loading ? <Spinner /> : <CurrentPostDetailsTables data={ postDetailsData } /> }
+			{ loading ? <Spinner /> : <CurrentPostDetailsSections data={ postDetailsData } /> }
 		</>
 	);
 }
 
 /**
- * Outputs all tables containing current post details.
+ * Outputs all the "Current Post Details" sections.
  *
- * @param {PostDetailsTableProps} props Props needed to populate the tables.
+ * @param {PostDetailsSectionProps} props Props needed to populate the sections.
  */
-function CurrentPostDetailsTables( props: PostDetailsTableProps ) {
+function CurrentPostDetailsSections( props: PostDetailsSectionProps ) {
 	const data = props.data;
 
 	return (
-		<div className="current-post-details">
-			{ GeneralPerformanceTable( data ) }
-			{ ReferrerTypesTable( data ) }
-			{ TopReferrersTable( data ) }
-			{ ActionsList( data ) }
+		<div className="current-post-details-panel">
+			{ DataPeriodSection( data ) }
+			{ GeneralPerformanceSection( data ) }
+			{ ReferrerTypesSection( data ) }
+			{ TopReferrersSection( data ) }
+			{ ActionsSection( data ) }
 		</div>
 	);
 }
 
 /**
- * Outputs the "General Performance" table.
+ * Outputs the "Period" section, which denotes the period for which data is
+ * shown.
  *
- * @param {PostPerformanceData} data The data needed to populate the table.
+ * @param {PostPerformanceData} data The data needed to populate the section.
  */
-function GeneralPerformanceTable( data: PostPerformanceData ) {
+function DataPeriodSection( data: PostPerformanceData ) {
+	const period = data.period;
+
+	// Get the date (in short format) on which the period starts.
+	const periodStart = Intl.DateTimeFormat(
+		document.documentElement.lang,
+		{ month: 'short', day: 'numeric' }
+	).format( new Date( period.start ) );
+
+	return (
+		<div className="section period">
+			{ __( 'Last ', 'wp-parsely' ) + period.days + __( ' Days ', 'wp-parsely' ) }
+			<span>
+				{ __( '(', 'wp-parsely' ) + periodStart + __( ' - Today)', 'wp-parsely' ) }
+			</span>
+		</div>
+	);
+}
+
+/**
+ * Outputs the "General Performance" (Views, Visitors, Time) section.
+ *
+ * @param {PostPerformanceData} data The data needed to populate the section.
+ */
+function GeneralPerformanceSection( data: PostPerformanceData ) {
 	return (
 		<div className="section general-performance">
-			<h2>{ __( 'General Performance (last 90 days)', 'wp-parsely' ) }</h2>
 			<table>
 				<tbody>
 					<tr>
-						<th>{ __( 'Views:', 'wp-parsely' ) }</th>
-						<td>{ data.views }</td>
-					</tr>
-					<tr>
-						<th>{ __( 'Visitors:', 'wp-parsely' ) }</th>
-						<td>{ data.visitors }</td>
-					</tr>
-					<tr>
-						<th title={
-							__( 'Average time visitors engaged with content', 'wp-parsely' )
-						}>{ __( 'Avg. Time:', 'wp-parsely' ) }</th>
+						<td>{ ImpreciseNumber( data.views ) }</td>
+						<td>{ ImpreciseNumber( data.visitors ) }</td>
 						<td>{ data.avgEngaged }</td>
 					</tr>
 				</tbody>
+				<tfoot>
+					<tr>
+						<th>{ __( 'Page Views', 'wp-parsely' ) }</th>
+						<th>{ __( 'Visitors', 'wp-parsely' ) }</th>
+						<th title={
+							__( 'Average time visitors engaged with content', 'wp-parsely' )
+						}>{ __( 'Engaged Time', 'wp-parsely' ) }</th>
+					</tr>
+				</tfoot>
 			</table>
 		</div>
 	);
 }
 
 /**
- * Outputs the "Post Performance" table.
+ * Outputs the "Referrer Types" section.
  *
- * @param {PostPerformanceData} data The data needed to populate the table.
+ * @param {PostPerformanceData} data The data needed to populate the section.
  */
-function ReferrerTypesTable( data: PostPerformanceData ) {
+function ReferrerTypesSection( data: PostPerformanceData ) {
+	// Remove unneeded totals to simplify upcoming map() calls.
+	delete data.referrers.types[ 'totals' as unknown as number ];
+
 	return (
 		<div className="section referrer-types">
-			<h2>{ __( 'Referrer Types (last 7 days)', 'wp-parsely' ) }</h2>
+			<div className="section-title">{ __( 'Referrers (Page Views)', 'wp-parsely' ) }</div>
+
+			<div className="multi-percentage-bar">{
+				Object.entries( data.referrers.types ).map( ( [ key, value ] ) => {
+					return (
+						<div className={ 'bar-fill ' + key } key={ key }
+							title={ value.viewsPercentage + __( '%', 'wp-parsely' ) }
+							style={ { width: value.viewsPercentage + '%' } }>
+						</div>
+					);
+				} ) }
+			</div>
+
 			<table>
 				<thead>
-					<tr>
-						<th>{ __( 'Referrer Type', 'wp-parsely' ) }</th>
-						<th colSpan={ 2 }>{ __( 'Views', 'wp-parsely' ) }</th>
+					<tr>{
+						Object.keys( data.referrers.types ).map( ( key ) => {
+							return <th key={ key }>{ key }</th>;
+						} ) }
 					</tr>
 				</thead>
 				<tbody>
-					{
+					<tr>{
 						Object.entries( data.referrers.types ).map( ( [ key, value ] ) => {
-							if ( key === 'totals' ) {
-								key = __( 'Total', 'wp-parsely' );
-								return (
-									<tr key={ key }>
-										<th>{ key + __( ':', 'wp-parsely' ) }</th>
-										<td>{ value.views }</td>
-									</tr>
-								);
-							}
-
-							return (
-								<tr key={ key }>
-									<th className="capitalized">{ key + __( ':', 'wp-parsely' ) }</th>
-									<td>{ value.views }</td>
-									<td>{ value.viewsPercentage + __( '%', 'wp-parsely' ) }</td>
-								</tr>
-							);
-						} )
-					}
+							return <td key={ key }>{ ImpreciseNumber( value.views ) }</td>;
+						} ) }
+					</tr>
 				</tbody>
 			</table>
 		</div>
@@ -168,69 +194,116 @@ function ReferrerTypesTable( data: PostPerformanceData ) {
 }
 
 /**
- * Outputs the "Top Referrers" table.
+ * Outputs the "Top Referrers" section.
  *
- * @param {PostPerformanceData} data The data needed to populate the table.
+ * @param {PostPerformanceData} data The data needed to populate the section.
  */
-function TopReferrersTable( data: PostPerformanceData ) {
-	let totalViewsPercentage;
+function TopReferrersSection( data: PostPerformanceData ) {
+	let totalViewsPercentage = 0;
 	return (
 		<div className="section top-referrers">
-			<h2>{ __( 'Top 5 Referrers (last 7 days)', 'wp-parsely' ) }</h2>
 			<table>
 				<thead>
 					<tr>
-						<th>{ __( 'Referrer', 'wp-parsely' ) }</th>
-						<th colSpan={ 2 }>{ __( 'Views', 'wp-parsely' ) }</th>
+						<th>{ __( 'Top Referrers', 'wp-parsely' ) }</th>
+						<th colSpan={ 2 }>{ __( 'Page Views', 'wp-parsely' ) }</th>
 					</tr>
 				</thead>
-				<tbody>
-					{
-						Object.entries( data.referrers.top ).map( ( [ key, value ] ) => {
-							if ( key === 'totals' ) {
-								key = __( 'Total', 'wp-parsely' );
-								totalViewsPercentage = value.viewsPercentage;
-								return (
-									<tr key={ key }>
-										<th title={ key }>{ key + __( ':', 'wp-parsely' ) }</th>
-										<td>{ value.views }</td>
-									</tr>
-								);
-							}
+				<tbody>{
+					Object.entries( data.referrers.top ).map( ( [ key, value ] ) => {
+						if ( key === 'totals' ) {
+							totalViewsPercentage = Number( value.viewsPercentage );
+							return null;
+						}
 
-							return (
-								<tr key={ key }>
-									<th title={ key }>{ key + __( ':', 'wp-parsely' ) }</th>
-									<td>{ value.views }</td>
-									<td>{ value.viewsPercentage + __( '%', 'wp-parsely' ) }</td>
-								</tr>
-							);
-						} )
-					}
+						return (
+							<tr key={ key }>
+								<th title={ key }>{ key }</th>
+								<td title={ value.viewsPercentage + __( '%', 'wp-parsely' ) }>
+									<div className="percentage-bar" style={ { '--bar-fill': value.viewsPercentage + '%' } as React.CSSProperties }></div>
+								</td>
+								<td>{ ImpreciseNumber( value.views ) }</td>
+							</tr>
+						);
+					} )
+				}
 				</tbody>
 			</table>
-			<p>
-				{ totalViewsPercentage + __( '% of all views came from the top 5 referrers.', 'wp-parsely' ) }
-			</p>
+			<div>
+				{ totalViewsPercentage.toFixed( 1 ) + __( '% of views came from top 5 referrers.', 'wp-parsely' ) }
+			</div>
 		</div>
 	);
 }
 
 /**
- * Outputs the "Actions" list.
+ * Outputs the "Actions" section.
  *
- * @param {PostPerformanceData} data The data needed to populate the list.
+ * @param {PostPerformanceData} data The data needed to populate the section.
  */
-function ActionsList( data: PostPerformanceData ) {
+function ActionsSection( data: PostPerformanceData ) {
 	return (
 		<div className="section actions">
-			<h2>{ __( 'Actions', 'wp-parsely' ) }</h2>
-			<ul>
-				<li><a href={ data.url } target="blank" rel="noopener">{ __( 'Visit Post', 'wp-parsely' ) }</a></li>
-				<li><a href={ data.statsUrl } target="blank" rel="noopener">{ __( 'View in the Parse.ly Dash', 'wp-parsely' ) }</a></li>
-			</ul>
+			<Button href={ data.url } rel="noopener" target="_blank" variant="secondary">{ __( 'Visit Post', 'wp-parsely' ) }</Button>
+			<Button href={ data.statsUrl } rel="noopener" target="_blank" variant="primary">{ __( 'View in Parse.ly', 'wp-parsely' ) }</Button>
 		</div>
 	);
+}
+
+/**
+ * Implements the "Imprecise Number" functionality of the Parse.ly dashboard.
+ *
+ * Note: This function is not made to process float numbers.
+ *
+ * @param {string} value          The number to process. It can be formatted.
+ * @param {number} fractionDigits The number of desired fraction digits.
+ * @param {string} glue           A string to put between the number and unit.
+ * @return {string} The number formatted as an imprecise number.
+ */
+function ImpreciseNumber( value: string, fractionDigits = 1, glue = '' ): string {
+	const number = parseInt( value.replace( /\D/g, '' ) );
+
+	if ( number < 1000 ) {
+		return value;
+	} else if ( number < 10000 ) {
+		fractionDigits = 1;
+	}
+
+	const unitNames = {
+		1000: 'k',
+		'1,000,000': 'M',
+		'1,000,000,000': 'B',
+		'1,000,000,000,000': 'T',
+		'1,000,000,000,000,000': 'Q',
+	};
+	let currentNumber = number;
+	let currentNumberAsString = number.toString();
+	let unit = '';
+	let previousNumber = 0;
+
+	Object.entries( unitNames ).forEach( ( [ thousands, suffix ] ) => {
+		const thousandsInt = parseInt( thousands.replace( /\D/g, '' ), 10 );
+
+		if ( number >= thousandsInt ) {
+			currentNumber = number / thousandsInt;
+			let precision = fractionDigits;
+
+			// For over 10 units, we reduce the precision to 1 fraction digit.
+			if ( currentNumber % 1 > 1 / previousNumber ) {
+				precision = currentNumber > 10 ? 1 : 2;
+			}
+
+			// Precision override, where we want to show 2 fraction digits.
+			const zeroes = parseFloat( currentNumber.toFixed( 2 ) ) === parseFloat( currentNumber.toFixed( 0 ) );
+			precision = zeroes ? 0 : precision;
+			currentNumberAsString = currentNumber.toFixed( precision );
+			unit = suffix;
+		}
+
+		previousNumber = thousandsInt;
+	} );
+
+	return currentNumberAsString + glue + unit;
 }
 
 export default CurrentPostDetails;
