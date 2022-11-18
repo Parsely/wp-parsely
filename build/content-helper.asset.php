e37e1cef52b072d6b6830a28d38845a0c51b9055
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-11-18 19:02:18 +0200

    Rebuild assets

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '224df6859b5a5b5472c6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '015b8cd5d39614f8fa43');
