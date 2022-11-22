7fe2dacd6d24d6983ae9b4adf5ad6561282f186d
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-11-22 18:09:15 +0200

    CH Current Post Details: Fix buttons border-radius

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '97add23ae616bb5d5d11');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '456b3df39c88ce0ff4a9');
