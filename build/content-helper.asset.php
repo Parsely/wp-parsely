b4592de775e9dc44e24ddadedbc87cd719d879df
mehmoodak <31419912+mehmoodak@users.noreply.github.com>
2022-11-29 17:15:32 +0500

    🐛 FIX: parsely analytics API tag query param

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'b0150c6183cdd1cee7e0');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '75bbe656d14a7c55f7cd');
