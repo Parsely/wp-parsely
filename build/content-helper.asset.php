92805d88423f346a8f6c436404a5ac9532c2ca55
mehmoodak <31419912+mehmoodak@users.noreply.github.com>
2022-12-07 10:18:18 +0500

    💫 UPDATE: convert px to rem

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'cea3e135db78452dfbf2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '745aa610238ff925c965');
