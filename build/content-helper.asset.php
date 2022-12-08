8d179f6368823434ce5ef2d95f1980fdd73610c5
mehmoodak <31419912+mehmoodak@users.noreply.github.com>
2022-12-08 15:05:33 +0500

    💫 UPDATE: build scripts

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '745aa610238ff925c965');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'b14f5a43a41ccdbb412e');
