ef60ee55d589c2e1cf7436ebba59818fca92aa04
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2022-12-08 19:55:51 +0500

    🐛 FIX: content helper top posts api request (#1237)

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'a33769e8a32bb2400f66');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '2790b97be9090bd9865e');
