da771459df06224f38c777109e670221992b3518
mehmoodak <31419912+mehmoodak@users.noreply.github.com>
2022-12-09 16:14:01 +0500

    👌 IMPROVE: adds separate types file instead of just using index

--- src/@types/assets/svg.d.ts
+++ src/@types/assets/svg.d.ts
@@ -4,8 +4,3 @@ declare module '\*.svg' {
 	const src: string;
 	export default src;
 }
-
-declare module '@wordpress/e2e-test-utils';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const page: any;
