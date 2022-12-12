559aa0b6b3227845d0bb4590b3ebf8b6e1b869f0
mehmoodak <31419912+mehmoodak@users.noreply.github.com>
2022-12-12 21:40:57 +0500

    💫 UPDATE: build files

--- build/loader.asset.php
+++ build/loader.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => '11d36be1108ae9f257cd');
+<?php return array('dependencies' => array('wp-hooks'), 'version' => '1a8322c7eac96ba3ef81');
