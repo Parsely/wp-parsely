ffcc2475e3ec6a439e5244e43e65c31d4ae05f0f
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-01-05 14:07:46 +0200

    CH Refactor: Convert most SASS variables to CSS variables

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '152fb01c38b4af8d220d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'e3397cbcbbc212ce6fd4');
