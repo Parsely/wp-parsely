8964e32f343f3790263bcc269137096cf6a96176
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-01-13 19:39:36 +0200

    Analytics Posts Proxy Endpoint: Add thumbnail URL field

--- src/Endpoints/class-analytics-posts-api-proxy.php
+++ src/Endpoints/class-analytics-posts-api-proxy.php
@@ -53,13 +53,14 @@ final class Analytics_Posts_API_Proxy extends Base_API_Proxy {
 		return array_map(
 			static function( stdClass $item ) use ( $date_format, $site_id ) {
 				return (object) array(
-					'author'  => $item->author,
-					'date'    => wp_date( $date_format, strtotime( $item->pub_date ) ),
-					'id'      => $item->url,
-					'dashUrl' => Parsely::get_dash_url( $site_id, $item->url ),
-					'title'   => $item->title,
-					'url'     => $item->url,
-					'views'   => $item->metrics->views,
+					'author'         => $item->author,
+					'date'           => wp_date( $date_format, strtotime( $item->pub_date ) ),
+					'id'             => $item->url,
+					'dashUrl'        => Parsely::get_dash_url( $site_id, $item->url ),
+					'thumbUrlMedium' => $item->thumb_url_medium,
+					'title'          => $item->title,
+					'url'            => $item->url,
+					'views'          => $item->metrics->views,
 				);
 			},
 			$response
