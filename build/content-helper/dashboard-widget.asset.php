557afb3225ec8f07be0c6afa19a22d526df031e7
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-01-19 16:24:33 +0200

    DW: Adjust initial height

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cbf2c7467da921da5365');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2e19b4c60df12f536784');
