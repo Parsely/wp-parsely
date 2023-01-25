e26dad7a229bedf3c104dd210442ecdcf6075cda
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2023-01-25 14:20:52 +0500

    Add Parse.ly Stats on List Screen of Posts, Pages and Custom Post Types (#1271)
    * ➕ ADDS: placeholder for parsely stats
    
    * ➕ ADDS: parsely stats column
    
    * ➕ ADDS: styles for parsely stats
    
    * ➕ ADDS: number and time formatting
    
    * ➕ ADDS: support for all track post types which can also include pages and custom post types
    
    * ➕ ADDS: unit and integeration tests

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -9,9 +9,18 @@ declare(strict_types=1);
 
 namespace Parsely\Tests\Integration;
 
+use DateTime;
+use DateTimeZone;
+use ReflectionClass;
+use ReflectionProperty;
+use ReflectionMethod;
 use Parsely\Parsely;
+use PHPUnit\Framework\RiskyTestError;
 use WP_Error;
+use WP_Post;
 use Yoast\WPTestUtils\WPIntegration\TestCase as WPIntegrationTestCase;
+
+use const Parsely\Utils\WP_DATE_TIME_FORMAT;
 
 /**
  * Abstract base class for all test case implementations.
@@ -169,6 +178,81 @@ abstract class TestCase extends WPIntegrationTestCase {
 	}
 
 	/**
+	 * Creates test posts in sequence.
+	 *
+	 * @param int    $num_of_posts Optional. Number of posts we need to create.
+	 * @param string $post_type Optional. Type of the posts.
+	 * @param string $post_status Optional. Status of the posts.
+	 *
+	 * @return WP_Post[]
+	 */
+	public function create_and_get_test_posts( int $num_of_posts = 1, $post_type = 'post', $post_status = 'publish' ) {
+		$post_ids = $this->create_posts_and_get_ids( $num_of_posts, $post_type, $post_status );
+
+		return $this->get_test_posts( $post_ids );
+	}
+
+	/**
+	 * Creates test posts in sequence.
+	 *
+	 * @param int    $num_of_posts Optional. Number of posts we need to create.
+	 * @param string $post_type Optional. Type of the posts.
+	 * @param string $post_status Optional. Status of the posts.
+	 *
+	 * @return int[]
+	 */
+	private function create_posts_and_get_ids( int $num_of_posts = 1, $post_type = 'post', $post_status = 'publish' ) {
+		/**
+		 * Variable.
+		 *
+		 * @var int[]
+		 */
+		$post_ids = array();
+		$date     = new DateTime( '2009-12-31', new DateTimeZone( 'America/New_York' ) ); // Date with timezone to replicate real world scenarios.
+
+		for ( $i = 1; $i <= $num_of_posts; $i++ ) {
+			$post_date = date_add( $date, date_interval_create_from_date_string( '1 days' ) ); // Increment by 1 day like sequence.
+			$post_id   = self::factory()->post->create(
+				array(
+					'post_type'     => $post_type,
+					'post_status'   => $post_status,
+					'post_title'    => "Title $i-($post_status)",
+					'post_author'   => $i,
+					'post_content'  => "Content $i",
+					'post_date'     => $post_date->format( WP_DATE_TIME_FORMAT ),
+					'post_date_gmt' => gmdate( WP_DATE_TIME_FORMAT, $post_date->getTimestamp() ),
+				)
+			);
+
+			array_push( $post_ids, $post_id );
+		}
+
+		return $post_ids;
+	}
+
+	/**
+	 * Gets given test posts.
+	 *
+	 * @param int[] $post_ids IDs of the posts.
+	 *
+	 * @return WP_Post[]
+	 */
+	private function get_test_posts( $post_ids = array() ) {
+		$posts = array();
+
+		foreach ( $post_ids as $post_id ) {
+			array_push( $posts, get_post( $post_id ) );
+		}
+
+		/**
+		 * Variable.
+		 *
+		 * @var WP_Post[]
+		 */
+		return $posts;
+	}
+
+	/**
 	 * Creates a new post and navigates to it.
 	 *
 	 * @param string $post_status Optional. The post's status. Default is 'publish'.
@@ -187,10 +271,232 @@ abstract class TestCase extends WPIntegrationTestCase {
 	 *
 	 * @param int $admin_user_id User ID for the site administrator.
 	 *                           Default is 1 which is assigned to first admin user while creating the site.
-	 *
-	 * @return void
 	 */
 	public function set_admin_user( $admin_user_id = 1 ): void {
 		wp_set_current_user( $admin_user_id );
+	}
+
+	/**
+	 * Verifies that given hooks are called or not.
+	 *
+	 * @param string[] $hooks WordPress hooks whose availability we have to verify.
+	 * @param bool     $availability_type TRUE if we want to check the presence of given hooks.
+	 */
+	public function assert_wp_hooks_availablility( $hooks, $availability_type ): void {
+		if ( ! $this->isPHPVersion7Dot2OrHigher() ) {
+			return;
+		}
+
+		if ( true === $availability_type ) {
+			$this->assert_wp_hooks( $hooks );
+		} else {
+			$this->assert_wp_hooks( array(), $hooks );
+		}
+	}
+
+	/**
+	 * Asserts WordPress hooks.
+	 *
+	 * @param string[] $true_hooks Optional. Actions that should have been present.
+	 * @param string[] $false_hooks Optional. Actions that should have not been present.
+	 *
+	 * @throws RiskyTestError If no assertions get passed to the function.
+	 */
+	private function assert_wp_hooks( array $true_hooks = array(), array $false_hooks = array() ): void {
+		if ( 0 === count( $true_hooks ) + count( $false_hooks ) ) {
+			throw new RiskyTestError( 'Function assert_wp_hooks() has been used without any arguments' );
+		}
+
+		foreach ( $true_hooks as $hook ) {
+			self::assertTrue(
+				has_action( $hook ),
+				"Unexpected hook status: $hook should have been called."
+			);
+		}
+
+		foreach ( $false_hooks as $hook ) {
+			self::assertFalse(
+				has_action( $hook ),
+				"Unexpected hook status: $hook should have not been called."
+			);
+		}
+	}
+
+	/**
+	 * Asserts that a passed script is not registered.
+	 *
+	 * @param string $handle Script handle to test.
+	 */
+	public function assert_is_script_not_registered( string $handle ): void {
+		$this->assert_script_statuses( $handle, array(), array( 'registered' ) );
+	}
+
+	/**
+	 * Asserts that a passed script is registered.
+	 *
+	 * @param string $handle Script handle to test.
+	 */
+	public function assert_is_script_registered( string $handle ): void {
+		$this->assert_script_statuses( $handle, array( 'registered' ) );
+	}
+
+	/**
+	 * Asserts that a passed script is not enqueued.
+	 *
+	 * @param string $handle Script handle to test.
+	 */
+	public function assert_is_script_not_enqueued( string $handle ): void {
+		$this->assert_script_statuses( $handle, array(), array( 'enqueued' ) );
+	}
+
+	/**
+	 * Asserts that a passed script is enqueued.
+	 *
+	 * @param string $handle Script handle to test.
+	 */
+	public function assert_is_script_enqueued( string $handle ): void {
+		$this->assert_script_statuses( $handle, array( 'enqueued' ) );
+	}
+
+	/**
+	 * Asserts multiple enqueuing statuses for a script.
+	 *
+	 * @param string $handle       Script handle to test.
+	 * @param array  $assert_true  Optional. Statuses that should assert to true. Accepts 'enqueued',
+	 *                             'registered', 'queue', 'to_do', and 'done'. Default is an empty array.
+	 * @param array  $assert_false Optional. Statuses that should assert to false. Accepts 'enqueued',
+	 *                             'registered', 'queue', 'to_do', and 'done'. Default is an empty array.
+	 *
+	 * @throws RiskyTestError If no assertions ($assert_true, $assert_false) get passed to the function.
+	 */
+	private function assert_script_statuses( string $handle, array $assert_true = array(), array $assert_false = array() ): void {
+		if ( 0 === count( $assert_true ) + count( $assert_false ) ) {
+			throw new RiskyTestError( 'Function assert_script_statuses() has been used without any arguments' );
+		}
+
+		foreach ( $assert_true as $status ) {
+			self::assertTrue(
+				wp_script_is( $handle, $status ),
+				"Unexpected script status: $handle status should be '$status'"
+			);
+		}
+
+		foreach ( $assert_false as $status ) {
+			self::assertFalse(
+				wp_script_is( $handle, $status ),
+				"Unexpected script status: $handle status should NOT be '$status'"
+			);
+		}
+	}
+
+	/**
+	 * Asserts that a passed style is not registered.
+	 *
+	 * @param string $handle Style handle to test.
+	 */
+	public function assert_is_style_not_registered( string $handle ): void {
+		$this->assert_style_statuses( $handle, array(), array( 'registered' ) );
+	}
+
+	/**
+	 * Asserts that a passed style is registered.
+	 *
+	 * @param string $handle Style handle to test.
+	 */
+	public function assert_is_style_registered( string $handle ): void {
+		$this->assert_style_statuses( $handle, array( 'registered' ) );
+	}
+
+	/**
+	 * Asserts that a passed style is not enqueued.
+	 *
+	 * @param string $handle Style handle to test.
+	 */
+	public function assert_is_style_not_enqueued( string $handle ): void {
+		$this->assert_style_statuses( $handle, array(), array( 'enqueued' ) );
+	}
+
+	/**
+	 * Asserts that a passed style is enqueued.
+	 *
+	 * @param string $handle Style handle to test.
+	 */
+	public function assert_is_style_enqueued( string $handle ): void {
+		$this->assert_style_statuses( $handle, array( 'enqueued' ) );
+	}
+
+	/**
+	 * Asserts multiple enqueuing statuses for a style.
+	 *
+	 * @param string $handle       Style handle to test.
+	 * @param array  $assert_true  Optional. Statuses that should assert to true. Accepts 'enqueued',
+	 *                             'registered', 'queue', 'to_do', and 'done'. Default is an empty array.
+	 * @param array  $assert_false Optional. Statuses that should assert to false. Accepts 'enqueued',
+	 *                             'registered', 'queue', 'to_do', and 'done'. Default is an empty array.
+	 *
+	 * @throws RiskyTestError If no assertions ($assert_true, $assert_false) get passed to the function.
+	 */
+	private function assert_style_statuses( string $handle, array $assert_true = array(), array $assert_false = array() ): void {
+		if ( 0 === count( $assert_true ) + count( $assert_false ) ) {
+			throw new RiskyTestError( 'Function assert_style_statuses() has been used without any arguments' );
+		}
+
+		foreach ( $assert_true as $status ) {
+			self::assertTrue(
+				wp_style_is( $handle, $status ),
+				"Unexpected style status: $handle status should be '$status'"
+			);
+		}
+
+		foreach ( $assert_false as $status ) {
+			self::assertFalse(
+				wp_style_is( $handle, $status ),
+				"Unexpected style status: $handle status should NOT be '$status'"
+			);
+		}
+	}
+
+	/**
+	 * Returns TRUE if minimum PHP version is 7.2 or higher. We uses this if something works
+	 * differently in PHP versions < 7.2 and >= 7.2.
+	 *
+	 * Note: Remove this function when we remove support for PHP 7.1.
+	 */
+	public function isPHPVersion7Dot2OrHigher(): bool {
+		return phpversion() >= '7.2';
+	}
+
+	/**
+	 * Gets private property of a class.
+	 *
+	 * @param class-string $class_name Name of the class.
+	 * @param string       $property_name Name of the property.
+	 *
+	 * @return ReflectionProperty
+	 */
+	public function get_private_property( $class_name, $property_name ) {
+		$reflector = new ReflectionClass( $class_name );
+		$property  = $reflector->getProperty( $property_name );
+
+		$property->setAccessible( true );
+
+		return $property;
+	}
+
+	/**
+	 * Gets private method of a class.
+	 *
+	 * @param class-string $class_name Name of the class.
+	 * @param string       $method Name of the method.
+	 *
+	 * @return ReflectionMethod
+	 */
+	public function get_private_method( $class_name, $method ) {
+		$reflector = new ReflectionClass( $class_name );
+		$method    = $reflector->getMethod( $method );
+
+		$method->setAccessible( true );
+
+		return $method;
 	}
 }
