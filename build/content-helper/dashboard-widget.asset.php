a4ce7902372f5c028c5a6a5834698add56ac6d6d
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-01-26 09:18:02 +0200

    CHDW: Improve SCSS

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2e19b4c60df12f536784');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a0a12f2f445db1ec0e71');
