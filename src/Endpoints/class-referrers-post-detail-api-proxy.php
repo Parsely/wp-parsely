80d248da1172b514be5a5025104a169342de208e
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-02-08 13:56:34 +0200

    CH: Fix top referrers percentage displaying as "NaN"

--- src/Endpoints/class-referrers-post-detail-api-proxy.php
+++ src/Endpoints/class-referrers-post-detail-api-proxy.php
@@ -46,7 +46,9 @@ final class Referrers_Post_Detail_API_Proxy extends Base_API_Proxy {
 	 * @return stdClass|WP_Error stdClass containing the data or a WP_Error object on failure.
 	 */
 	public function get_items( WP_REST_Request $request ) {
-		$this->total_views = absint( $request->get_param( 'total_views' ) );
+		$this->total_views = $this->convert_to_positive_integer(
+			strval( $request->get_param( 'total_views' ) )
+		);
 		$request->offsetUnset( 'total_views' ); // Remove param from request.
 		return $this->get_data( $request );
 	}
@@ -62,7 +64,9 @@ final class Referrers_Post_Detail_API_Proxy extends Base_API_Proxy {
 	 */
 	protected function generate_data( $response ): array {
 		$referrers_types = $this->generate_referrer_types_data( $response );
-		$direct_views    = absint( preg_replace( '/\D/', '', $referrers_types->direct->views ) );
+		$direct_views    = $this->convert_to_positive_integer(
+			$referrers_types->direct->views
+		);
 		$referrers_top   = $this->generate_referrers_data( 5, $response, $direct_views );
 
 		return array(
@@ -210,6 +214,17 @@ final class Referrers_Post_Detail_API_Proxy extends Base_API_Proxy {
 		}
 
 		return $result;
+	}
+
+	/**
+	 * Converts a string to a positive integer, removing any non-numeric
+	 * characters.
+	 *
+	 * @param string $string The string to be converted to an integer.
+	 * @return int The integer resulting from the conversion.
+	 */
+	private function convert_to_positive_integer( string $string ): int {
+		return (int) preg_replace( '/\D/', '', $string );
 	}
 
 	/**
