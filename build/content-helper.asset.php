5101a2fa9597db3f10b29f0bd74d62529e2714be
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2023-03-03 16:24:55 +0500

    🐛 FIX:  lint issues in dashboard widget

--- build/content-helper.asset.php
+++ build/content-helper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '157f0f835316a7b193df');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '82a90d4fc94dfd8e3e28');
