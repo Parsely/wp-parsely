87a5b0fd12bb507eb4b74dc14cd9881481e168ad
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2023-03-03 22:40:35 +0500

    keep custom date functions

--- src/blocks/shared/utils/date.ts
+++ src/blocks/shared/utils/date.ts
@@ -27,3 +27,28 @@ export function getSmartShortDate( date: Date ): string {
 		dateFormat
 	).format( date );
 }
+
+/**
+ * Removes the given number of days from a "YYYY-MM-DD" string, and returns
+ * the result in the same format.
+ *
+ * @param {string} date The date in "YYYY-MM-DD" format.
+ * @param {number} days The number of days to remove from the date.
+ * @return {string} The resulting date in "YYYY-MM-DD" format.
+ */
+export function removeDaysFromDate( date: string, days: number ): string {
+	const pastDate = new Date( date );
+	pastDate.setDate( pastDate.getDate() - days );
+
+	return convertDateToString( pastDate );
+}
+
+/**
+ * Converts a date to a string in "YYYY-MM-DD" format.
+ *
+ * @param {Date} date The  date to format.
+ * @return {string} The date in "YYYY-MM-DD" format.
+ */
+export function convertDateToString( date: Date ): string {
+	return date.toISOString().substring( 0, 10 );
+}
