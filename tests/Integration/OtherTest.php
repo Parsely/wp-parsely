6e8ad656004d76e18294293093b31d423222dc5e
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-05-05 15:09:28 +0300

    Fix integration test bug

--- tests/Integration/OtherTest.php
+++ tests/Integration/OtherTest.php
@@ -36,7 +36,14 @@ final class OtherTest extends TestCase {
 		$wp_scripts    = new WP_Scripts();
 		self::$parsely = new Parsely();
 
-		TestCase::set_options();
+		// Reset all credentials to empty values.
+		self::set_options(
+			array(
+				'apikey'          => '',
+				'api_secret'      => '',
+				'metadata_secret' => '',
+			)
+		);
 	}
 
 	/**
@@ -204,7 +211,6 @@ final class OtherTest extends TestCase {
 	 * @uses \Parsely\Parsely::get_options
 	 */
 	public function test_checking_site_id_is_set_or_not(): void {
-		self::set_options( array( 'apikey' => '' ) );
 		self::assertFalse( self::$parsely->site_id_is_set() );
 		self::assertTrue( self::$parsely->site_id_is_missing() );
 
@@ -313,10 +319,9 @@ final class OtherTest extends TestCase {
 	 * @uses \Parsely\Parsely::get_options
 	 */
 	public function test_can_retrieve_site_id(): void {
+		self::assertSame( '', self::$parsely->get_site_id() );
 		self::set_options( array( 'apikey' => 'somekey' ) );
 		self::assertSame( 'somekey', self::$parsely->get_site_id() );
-		self::set_options( array( 'apikey' => '' ) );
-		self::assertSame( '', self::$parsely->get_site_id() );
 	}
 
 	/**
@@ -382,6 +387,7 @@ final class OtherTest extends TestCase {
 	 * @uses \Parsely\Parsely::get_options
 	 */
 	public function test_get_tracker_url(): void {
+		self::set_options( array( 'apikey' => 'blog.parsely.com' ) );
 		$expected = 'https://cdn.parsely.com/keys/blog.parsely.com/p.js';
 		self::assertEquals( $expected, self::$parsely->get_tracker_url() );
 	}
@@ -397,7 +403,6 @@ final class OtherTest extends TestCase {
 	 * @uses \Parsely\Parsely::get_options
 	 */
 	public function test_get_tracker_no_site_id(): void {
-		self::set_options( array( 'apikey' => '' ) );
 		$expected = '';
 		self::assertEquals( $expected, self::$parsely->get_tracker_url() );
 	}
