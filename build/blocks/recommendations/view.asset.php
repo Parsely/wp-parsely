14be23f8f98ce936746e1ec0563a92ace6dd16f3
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-05-30 10:55:09 +0300

    Recommendations Block: Fix endless API request loop

--- build/blocks/recommendations/view.asset.php
+++ build/blocks/recommendations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '49d5733738465b1d2e30');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e7e23fd0ef67714b7483');
