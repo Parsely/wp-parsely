f18ed3bc0dffa0f175d00af733e67bd8817db0c9
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-06-19 14:44:39 +0300

    Fix some "code smells" flagged by SonarCloud

--- build/blocks/recommendations/edit.asset.php
+++ build/blocks/recommendations/edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2e99c7f2b2fb8f358f79');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f2b48f4ca8d5adef8a21');
--- build/blocks/recommendations/view.asset.php
+++ build/blocks/recommendations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6bdb01351923984b4368');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b16762e8d9612edb02c6');
