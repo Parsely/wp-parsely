10f3b1b4e7f7adf0b251bf95eb140e772d07743f
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-07-04 16:17:24 +0300

    OptionsTest: Add uses comments to tests

--- tests/Integration/OptionsTest.php
+++ tests/Integration/OptionsTest.php
@@ -37,9 +37,11 @@ final class OptionsTest extends TestCase {
 	 * @since 3.0.0
 	 *
 	 * @covers \Parsely\Parsely::get_options
+	 * @uses \Parsely\Parsely::get_managed_credentials
+	 * @uses \Parsely\Parsely::set_default_track_as_values
 	 */
 	public function test_default_options_are_returned_when_options_are_corrupted_or_not_set(): void {
-		update_option( Parsely::OPTIONS_KEY, 'someinvalidvalue' );
+		add_option( Parsely::OPTIONS_KEY, 'someinvalidvalue' );
 
 		$options = self::$parsely->get_options();
 		self::assertSame( self::$parsely->get_default_options(), $options );
@@ -54,15 +56,11 @@ final class OptionsTest extends TestCase {
 	 * @since 3.9.0
 	 *
 	 * @covers Parsely\Parsely::get_options
+	 * @uses \Parsely\Parsely::get_managed_credentials
+	 * @uses \Parsely\Parsely::set_default_track_as_values
 	 */
 	public function test_get_options_returns_correct_track_as_defaults(): void {
 		$options = self::$parsely->get_options();
-		self::assertSame( array( 'post' ), $options['track_post_types'] );
-		self::assertSame( array( 'page' ), $options['track_page_types'] );
-
-		delete_option( Parsely::OPTIONS_KEY );
-		$options = self::$parsely->get_options();
-
 		self::assertSame( array( 'post' ), $options['track_post_types'] );
 		self::assertSame( array( 'page' ), $options['track_page_types'] );
 	}
@@ -74,102 +72,73 @@ final class OptionsTest extends TestCase {
 	 * @since 3.9.0
 	 *
 	 * @covers Parsely\Parsely::get_options
+	 * @uses \Parsely\Parsely::get_managed_credentials
+	 * @uses \Parsely\Parsely::set_default_track_as_values
 	 */
 	public function test_get_options_track_as_defaults_when_cpts_are_registered(): void {
-		// Post.
-		register_post_type(
-			'custom_post_type',
-			array(
+		$custom_post_types = array(
+			// Post.
+			'custom_post_type'     => array(
 				'public'       => true,
 				'supports'     => array( 'editor' ),
 				'hierarchical' => false,
-			)
-		);
-
-		// Private post.
-		register_post_type(
-			'excluded_post_type_1',
-			array(
+			),
+			// Private post.
+			'excluded_post_type_1' => array(
 				'public'       => false,
 				'supports'     => array( 'editor' ),
 				'hierarchical' => false,
-			)
-		);
-
-		// Post without editor support.
-		register_post_type(
-			'excluded_post_type_2',
-			array(
+			),
+			// Post without editor support.
+			'excluded_post_type_2' => array(
 				'public'       => true,
 				'supports'     => array( 'editor' => false ),
 				'hierarchical' => false,
-			)
-		);
-
-		// Private post without editor support.
-		register_post_type(
-			'excluded_post_type_3',
-			array(
+			),
+			// Private post without editor support.
+			'excluded_post_type_3' => array(
 				'public'       => false,
 				'supports'     => array( 'editor' => false ),
 				'hierarchical' => false,
-			)
-		);
-
-		// Page.
-		register_post_type(
-			'custom_page_type',
-			array(
+			),
+			// Page.
+			'custom_page_type'     => array(
 				'public'       => true,
 				'supports'     => array( 'editor' ),
 				'hierarchical' => true,
-			)
-		);
-
-		// Private page.
-		register_post_type(
-			'excluded_page_type_1',
-			array(
+			),
+			// Private page.
+			'excluded_page_type_1' => array(
 				'public'       => false,
 				'supports'     => array( 'editor' ),
 				'hierarchical' => true,
-			)
-		);
-
-		// Page without editor support.
-		register_post_type(
-			'excluded_page_type_2',
-			array(
+			),
+			// Page without editor support.
+			'excluded_page_type_2' => array(
 				'public'       => true,
 				'supports'     => array( 'editor' => false ),
 				'hierarchical' => true,
-			)
-		);
-
-		// Private page without editor support.
-		register_post_type(
-			'excluded_page_type_3',
-			array(
+			),
+			// Private page without editor support.
+			'excluded_page_type_3' => array(
 				'public'       => false,
 				'supports'     => array( 'editor' => false ),
 				'hierarchical' => true,
-			)
+			),
 		);
 
-		delete_option( Parsely::OPTIONS_KEY );
+		foreach ( $custom_post_types as $key => $value ) {
+			register_post_type( $key, $value );
+		}
+
 		$options = self::$parsely->get_options();
 
 		self::assertSame( array( 'post', 'custom_post_type' ), $options['track_post_types'] );
 		self::assertSame( array( 'page', 'custom_page_type' ), $options['track_page_types'] );
 
 		// Clean up.
-		unregister_post_type( 'custom_post_type' );
-		unregister_post_type( 'excluded_post_type_1' );
-		unregister_post_type( 'excluded_post_type_2' );
-		unregister_post_type( 'excluded_post_type_3' );
-		unregister_post_type( 'custom_page_type' );
-		unregister_post_type( 'excluded_page_type_1' );
-		unregister_post_type( 'excluded_post_type_2' );
-		unregister_post_type( 'excluded_post_type_3' );
+		foreach ( $custom_post_types as $key => $value ) {
+			unregister_post_type( $key );
+		}
 	}
 }
