0e93b15525f9c7a7ed801529f60d4385bea0a262
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-08-10 12:53:29 +0300

    jest-e2e.config.js: Fix comment capitalization

--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -3,5 +3,5 @@ const baseConfig = require( '@wordpress/scripts/config/jest-e2e.config' );
 
 module.exports = {
 	...baseConfig,
-	testTimeout: 35000, // Increased timeout for e2e tests.
+	testTimeout: 35000, // Increased timeout for E2E tests.
 };
