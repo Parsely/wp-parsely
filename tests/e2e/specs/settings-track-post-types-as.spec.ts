9d142ffc1bd107f427152bee7cef069223387c2b
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-08-15 18:43:26 +0300

    Revert "Fix E2E test failure - Attempt 4"
    This reverts commit 076e581f411ffe767dfeb2f7cc2c44132e50df1a.

--- tests/e2e/specs/settings-track-post-types-as.spec.ts
+++ tests/e2e/specs/settings-track-post-types-as.spec.ts
@@ -29,7 +29,7 @@ describe( 'Track Post Types as', () => {
 	/**
 	 * Login, activate the Parse.ly plugin and show recrawl settings.
 	 */
-	beforeEach( async () => {
+	beforeAll( async () => {
 		await visitAdminPage( '/options-general.php', '?page=parsely' );
 		await page.click( '.recrawl-section-tab' );
 	} );
@@ -37,7 +37,7 @@ describe( 'Track Post Types as', () => {
 	/**
 	 * Set default values and save.
 	 */
-	afterEach( async () => {
+	afterAll( async () => {
 		await waitForWpAdmin();
 
 		await page.click( radioPostAsPost );
