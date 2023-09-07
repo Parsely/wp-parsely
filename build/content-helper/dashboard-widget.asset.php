84a8a3b19629faa196b474d028fbc928ec0e7b01
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-09-07 16:40:36 +0300

    Rebuild assets

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '62d8f688c01c5ffe3877');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b0d97cf645a84579ff2d');
