15c60d265e3d528140a81984b6a38e0ffb5c5b8c
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-09-25 09:46:39 +0300

    Update wp-parsely version to 3.10.0

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -8,7 +8,7 @@ import {
 	visitAdminPage,
 } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '3.9.1';
+export const PLUGIN_VERSION = '3.10.0';
 export const VALID_API_SECRET = 'valid_api_secret_key_based_on_length';
 
 export const waitForWpAdmin = () => page.waitForSelector( 'body.wp-admin' );
