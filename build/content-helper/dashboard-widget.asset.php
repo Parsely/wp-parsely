51fd727670fe3cad49fd097cd721aef9c725325d
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-10-03 18:49:09 +0300

    PCH Sidebar: Add period and metric settings

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '12c51b20178160c02ccc');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9f91c61f26036c1b1899');
