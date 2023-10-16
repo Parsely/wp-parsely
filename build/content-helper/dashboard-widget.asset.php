4eb922fdfccae4c08ea26ff66faa534e4db07e8b
Henrique Mouta <henrique@automattic.com>
2023-10-16 14:58:40 +0100

    Building again

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '5f4a4c1db21f14c7261e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b01850623223338213ce');
