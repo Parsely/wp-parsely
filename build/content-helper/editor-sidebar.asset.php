dfaf24e058ab79b0dd6af325a63aa63c82e75af6
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-10-18 10:52:01 +0300

    PCH Related Top Posts: Use screen-reader-text instead of title

--- build/content-helper/editor-sidebar.asset.php
+++ build/content-helper/editor-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '38150215790573524060');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'cb07876fd9c8e3e4c865');
