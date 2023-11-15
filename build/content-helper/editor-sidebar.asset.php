071603ecee457b867ba1e59f3e4f490b1db87a97
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2023-11-15 17:49:21 +0200

    Title Suggestions: Adjust title buttons position and color

--- build/content-helper/editor-sidebar.asset.php
+++ build/content-helper/editor-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '5264e3d08c49be05bd5c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '87489b9cef1578cad771');
