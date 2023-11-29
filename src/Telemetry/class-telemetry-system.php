064016e9261f1fcfbca36d1440d6be16dffcb231
Henrique Mouta <henrique@automattic.com>
2023-11-29 14:20:32 +0000

    Make sure the JS telemetry library is always available, but the tracking object is only available if telemetry is turned on

--- src/Telemetry/class-telemetry-system.php
+++ src/Telemetry/class-telemetry-system.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
 namespace Parsely\Telemetry;
 
 use WP_Error;
+use function Parsely\Utils\get_asset_info;
+use const Parsely\PARSELY_FILE;
+use const Parsely\PARSELY_VERSION;
 
 /**
  * Base class for all telemetry system implementations.
@@ -83,4 +86,94 @@ abstract class Telemetry_System {
 		string $event_name,
 		array $event_properties = array()
 	);
+
+	/**
+	 * Initializes JavaScript tracking.
+	 *
+	 * This method is responsible for setting up the JavaScript tracking for the application.
+	 * It enqueues the necessary scripts and sets up the parameters for the tracking script.
+	 *
+	 * If the user has disabled wp-admin telemetry, the script will be enqueued, however the
+	 * global object `wpParselyTra§cksTelemetry` will not be available.
+	 *
+	 * @since 3.12.0
+	 */
+	public static function init_js_tracking(): void {
+		// Enqueue the JS file.
+		add_action(
+			'admin_enqueue_scripts',
+			function (): void {
+				$asset_php        = get_asset_info( 'build/telemetry.asset.php' );
+				$built_assets_url = plugin_dir_url( PARSELY_FILE ) . 'build/';
+
+				// The Telemetry script is always enqueued in the admin.
+				// If the user has disabled wp-admin telemetry, the global object will not be available.
+				wp_enqueue_script(
+					'wp-parsely-tracks-telemetry',
+					$built_assets_url . 'telemetry.js',
+					$asset_php['dependencies'],
+					$asset_php['version'],
+					true
+				);
+
+				// If the user has disabled wp-admin telemetry, return early.
+				if ( ! Telemetry_System::is_wpadmin_telemetry_allowed() ) {
+					return;
+				}
+
+				// Set the script params.
+				$script_params = array(
+					'version' => PARSELY_VERSION,
+					'user'    => array(),
+				);
+
+				// If it's a VIP environment, add the VIP environment to the script params.
+				if ( defined( 'VIP_GO_APP_ENVIRONMENT' ) ) {
+					$app_environment = constant( 'VIP_GO_APP_ENVIRONMENT' );
+					if ( is_string( $app_environment ) && '' !== $app_environment ) {
+						$script_params['vipgo_env'] = $app_environment;
+					}
+				}
+
+				// Define user-specific params.
+				$wp_user_id = get_current_user_id();
+				if ( 0 !== $wp_user_id ) {
+					// If it's VIP environment, add the VIP user ID to the script params.
+					if ( defined( 'VIP_GO_APP_ID' ) ) {
+						$app_id = constant( 'VIP_GO_APP_ID' );
+						if ( is_integer( $app_id ) && 0 < $app_id ) {
+							$script_params['user'] = array(
+								'type' => 'vip_go_app_wp_user',
+								'id'   => $app_id . '_' . $wp_user_id,
+							);
+						}
+					}
+
+					// If not VIP, fallback to the generated parse.ly user ID.
+					if ( 0 === count( $script_params['user'] ) ) {
+						$wp_base_url = get_option( 'home' );
+						if ( ! is_string( $wp_base_url ) || '' === $wp_base_url ) {
+							$wp_base_url = get_option( 'siteurl' );
+						}
+
+						/**
+						 * The base URL of the site.
+						 *
+						 * @var string $wp_base_url
+						 */
+						$script_params['user'] = array(
+							'type' => 'wpparsely:user_id',
+							'id'   => wp_hash( sprintf( '%s|%s', $wp_base_url, $wp_user_id ) ),
+						);
+					}
+				}
+
+				wp_add_inline_script(
+					'wp-parsely-tracks-telemetry',
+					'const wpParselyTracksTelemetry = ' . wp_json_encode( $script_params ) . ';',
+					'before'
+				);
+			}
+		);
+	}
 }
