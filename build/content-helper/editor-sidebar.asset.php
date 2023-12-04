c3ecd34fe3b6aa846b10accc368601761ce00120
Henrique Mouta <henrique@automattic.com>
2023-12-04 10:51:39 +0000

    Update built files

--- build/content-helper/editor-sidebar.asset.php
+++ build/content-helper/editor-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'd10a7d502fbda865d8ec');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'e34eca7aa2ef4c9cfc5a');
