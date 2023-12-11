93afc2db3a156c501a17177382f494fd01d127a2
Henrique Mouta <henrique@automattic.com>
2023-12-11 13:56:53 +0000

    Add integration test for `Suggest_Meta_Description_API` class

--- tests/Integration/RemoteAPI/content-suggestions/WriteTitleAPITest.php
+++ tests/Integration/RemoteAPI/content-suggestions/WriteTitleAPITest.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace Parsely\Tests\Integration\RemoteAPI\ContentSuggestions;
 
 use Parsely\Parsely;
+use Parsely\RemoteAPI\ContentSuggestions\Suggest_Meta_Description_API;
 use Parsely\RemoteAPI\ContentSuggestions\Write_Title_API;
 use Parsely\Tests\Integration\RemoteAPITest;
 
@@ -83,11 +84,10 @@ final class WriteTitleAPITest extends RemoteAPITest {
 		}
 
 		$response = array(
-			'code'  => 200,
 			'titles' => array(
-				'Lorem Ipsum is a random title',
-				'Write better with Lorem Ipsum',
-				'Lorem Ispsum? Dolor sit amet!',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+				'Donec maximus metus sed urna maximus, et malesuada dui placerat.',
+				'Donec risus dui, dictum nec interdum eu, malesuada non diam.',
 			),
 		);
 
@@ -95,7 +95,10 @@ final class WriteTitleAPITest extends RemoteAPITest {
 			'headers'     => array(),
 			'cookies'     => array(),
 			'filename'    => null,
-			'response'    => $response,
+			'response'    => array(
+				'code'  => 200,
+				'message' => 'OK',
+			),
 			'status_code' => 200,
 			'success'     => true,
 			'body'        => $this->wp_json_encode( $response ),
