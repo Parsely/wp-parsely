f261f862982cbb0066bdd036243528a593e32c22
Henrique Mouta <henrique@automattic.com>
2024-01-11 10:39:27 +0000

    Fix condition typo

--- src/Utils/utils.php
+++ src/Utils/utils.php
@@ -355,7 +355,7 @@ function get_asset_info( string $path ) {
  * @return bool Returns true if $haystack starts with $needle, false otherwise.
  */
 function str_starts_with( string $haystack, string $needle ): bool {
-	if ( ! function_exists( '\str_starts_with' ) ) {
+	if ( function_exists( '\str_starts_with' ) ) {
 		return \str_starts_with( $haystack, $needle );
 	}
 	return 0 === strpos( (string) $haystack, (string) $needle );
