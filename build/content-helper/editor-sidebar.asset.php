1184f2931c04aaf825d04498a53ea3cf4b8de420
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-01-15 16:24:01 +0200

    PCH Title Suggestions: Remove tone and persona from store

--- build/content-helper/editor-sidebar.asset.php
+++ build/content-helper/editor-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '499d3ca390f6780cc6b5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '87b2ffe95f05776cd359');
