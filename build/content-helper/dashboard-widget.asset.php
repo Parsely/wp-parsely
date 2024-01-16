3f08cf13d80a1ef3e1459880bf261a415270a8fb
Henrique Mouta <henrique@automattic.com>
2024-01-16 11:17:50 +0000

    Fix singular translation issues & accessibility improvements  (#2119)
    * Fix PHPStan issue
    
    * Add area labels to previous and next navigations on the Dashboard Widget
    
    * Remove unnecessary translatable string
    
    * Change singular translations on getPeriodDescription() function
    
    * Add build files
    
    * Used, Yoda conditions must be.
    
    ---------
    
    Co-authored-by: Alex Cicovic <23142906+acicovic@users.noreply.github.com>

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'eb2c678672339effd351');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'bc8a9708da1acc75b14d');
