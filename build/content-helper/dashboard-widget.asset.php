0610c4b049d20ab668d5f7f4c139053819bc319e
Henrique Mouta <henrique@automattic.com>
2024-02-06 16:55:10 +0000

    Add missing build files.

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7ec1b1420e0a0fd65141');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '42479f5bb8a40eb92d78');
