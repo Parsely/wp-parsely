615a14d1e1410e0fc333a07797b99a6b839f6d06
Henrique Mouta <henrique@automattic.com>
2024-02-08 15:32:42 +0000

    Add built files

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e0736d6c6880c0359ab0');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd8580111d7561e98c2de');
