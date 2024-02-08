51da7939f786a29a648de73df7735db874e0e799
Henrique Mouta <henrique@automattic.com>
2024-02-08 17:08:37 +0000

    Add built files

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9e7dad3e4ae68260ca90');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0cf2560f2bdc0dd6c9f5');
--- build/content-helper/editor-sidebar.asset.php
+++ build/content-helper/editor-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'e483e00d9b2379a86c69');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '140f15451a59c5e48987');
