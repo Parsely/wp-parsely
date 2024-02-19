3b08054b214c14edadcc26c23e2f6502b34cd225
Henrique Mouta <henrique@automattic.com>
2024-02-19 13:06:58 +0000

    Make error messages more descriptive for common API errors

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '129dbcef4fff1d8e5eb3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9c5516ceea8d7a12b124');
--- build/content-helper/excerpt-generator.asset.php
+++ build/content-helper/excerpt-generator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-url', 'wp-wordcount'), 'version' => 'd64fdc33f439dcc40611');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-url', 'wp-wordcount'), 'version' => '69f459c343bb1f8f0e2d');
