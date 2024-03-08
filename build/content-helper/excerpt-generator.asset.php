401e177bb195cfd103d074c72da1b2b4b6e238c8
Henrique Mouta <henrique@automattic.com>
2024-03-08 13:08:53 +0000

    Delete build files

--- build/content-helper/excerpt-generator.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => 'ea68bf618136df730e5a');
--- build/telemetry.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-data', 'wp-element', 'wp-plugins'), 'version' => '77eca9d53acf3533f9f9');
