7457e25961c12e3834b7e3576c41c8ce9eb3b305
Henrique Mouta <henrique@automattic.com>
2024-03-08 09:47:55 +0000

    PCH Smart Linking: Update UI (#2268)
    * Redesign Smart Linking panels
    
    * Complete some DocBlocks
    
    ---------
    
    Co-authored-by: Alex Cicovic <23142906+acicovic@users.noreply.github.com>

--- build/content-helper/editor-sidebar.asset.php
+++ build/content-helper/editor-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '537f8847e59141224baa');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'ea850f58cd95403acde8');
