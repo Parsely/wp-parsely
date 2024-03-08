0df67871a4954e5b1192b58e4e6fad8d81ef76be
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-03-08 16:22:45 +0200

    Update Content Suggestions API Beta message

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '136ae706ae2ab6e091ad');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '68d9813064fecfe775a1');
