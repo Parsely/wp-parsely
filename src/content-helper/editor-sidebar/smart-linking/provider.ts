a3c73fd492ce09988c00b40a6403f48d7fb93a0a
Henrique Mouta <henrique@automattic.com>
2024-04-05 13:18:52 +0100

    Implement retry logic on Smart Linking API requests

--- src/content-helper/editor-sidebar/smart-linking/provider.ts
+++ src/content-helper/editor-sidebar/smart-linking/provider.ts
@@ -42,6 +42,23 @@ interface SmartLinkingApiResponse {
  */
 export class SmartLinkingProvider {
 	/**
+	 * The AbortController instance used to cancel the fetch request.
+	 *
+	 * @since 3.15.0
+	 */
+	private static abortController: AbortController = new AbortController();
+
+	/**
+	 * Cancels the fetch request.
+	 *
+	 * @since 3.15.0
+	 */
+	static cancelRequest(): void {
+		SmartLinkingProvider.abortController.abort();
+		SmartLinkingProvider.abortController = new AbortController();
+	}
+
+	/**
 	 * Returns a list of suggested links for the given content.
 	 *
 	 * @param {string}   content          The content to generate links for.
@@ -69,8 +86,17 @@ export class SmartLinkingProvider {
 					url_exclusion_list: urlExclusionList,
 					text: content,
 				},
+				signal: SmartLinkingProvider.abortController.signal,
 			} );
 		} catch ( wpError: any ) { // eslint-disable-line @typescript-eslint/no-explicit-any
+			if ( wpError.name === 'AbortError' ) {
+				return Promise.reject(
+					new ContentHelperError(
+						'The operation was aborted.',
+						ContentHelperErrorCode.ParselyAborted,
+					),
+				);
+			}
 			return Promise.reject( new ContentHelperError( wpError.message, wpError.code ) );
 		}
 
