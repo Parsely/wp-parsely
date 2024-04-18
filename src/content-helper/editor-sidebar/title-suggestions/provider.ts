de917f49402dfd0322f8b8d8a8d587dcc88bc09b
Henrique Mouta <henrique@automattic.com>
2024-04-18 14:27:23 +0100

    Apply new BaseProvider to TitleSuggestionsProvider

--- src/content-helper/editor-sidebar/title-suggestions/provider.ts
+++ src/content-helper/editor-sidebar/title-suggestions/provider.ts
@@ -1,29 +1,14 @@
 /**
  * WordPress dependencies
  */
-import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal dependencies
  */
-import {
-	ContentHelperError,
-	ContentHelperErrorCode,
-} from '../../common/content-helper-error';
+import { BaseProvider } from '../../common/base-provider';
 import { getToneLabel, ToneProp } from '../../common/components/tone-selector';
 import { getPersonaLabel, PersonaProp } from '../../common/components/persona-selector';
-
-/**
- * Specifies the form of the response returned by the
- * `content-suggestions/suggest-headline` WordPress REST API endpoint.
- *
- * @since 3.12.0
- */
-interface SuggestHeadlineApiResponse {
-	error?: Error;
-	data: string[],
-}
 
 /**
  * Returns data from the `content-suggestions/suggest-headline` WordPress REST API
@@ -31,45 +16,30 @@ interface SuggestHeadlineApiResponse {
  *
  * @since 3.12.0
  */
-export class TitleSuggestionsProvider {
+export class TitleSuggestionsProvider extends BaseProvider {
 	/**
 	 * Returns a list of suggested titles for the given content.
 	 *
-	 * @param {string }     content The content to generate titles for.
-	 * @param {number}      limit   The number of titles to return. Defaults to 3.
-	 * @param {ToneProp}    tone    The tone to use when generating the titles.
-	 * @param {PersonaProp} persona The persona to use when generating the titles.
+	 * @param { string }      content The content to generate titles for.
+	 * @param { number }      limit   The number of titles to return. Defaults to 3.
+	 * @param { ToneProp }    tone    The tone to use when generating the titles.
+	 * @param { PersonaProp } persona The persona to use when generating the titles.
 	 *
-	 * @return { Promise<string[]>} The resulting list of titles.
+	 * @return { Promise<string[]> } The resulting list of titles.
 	 */
 	public async generateTitles( content: string, limit: number = 3, tone: ToneProp, persona: PersonaProp ): Promise<string[]> {
-		let response;
+		const response = BaseProvider.fetch<string[]>( {
+			method: 'POST',
+			path: addQueryArgs( '/wp-parsely/v1/content-suggestions/suggest-headline', {
+				limit,
+				tone: getToneLabel( tone ),
+				persona: getPersonaLabel( persona ),
+			} ),
+			data: {
+				content,
+			},
+		} );
 
-		try {
-			response = await apiFetch<SuggestHeadlineApiResponse>( {
-				method: 'POST',
-				path: addQueryArgs( '/wp-parsely/v1/content-suggestions/suggest-headline', {
-					limit,
-					tone: getToneLabel( tone ),
-					persona: getPersonaLabel( persona ),
-				}, ),
-				data: {
-					content,
-				},
-			} );
-		} catch ( wpError: any ) { // eslint-disable-line @typescript-eslint/no-explicit-any
-			return Promise.reject( new ContentHelperError(
-				wpError.message, wpError.code
-			) );
-		}
-
-		if ( response?.error ) {
-			return Promise.reject( new ContentHelperError(
-				response.error.message,
-				ContentHelperErrorCode.ParselyApiResponseContainsError
-			) );
-		}
-
-		return response?.data ?? [];
+		return response ?? [];
 	}
 }
