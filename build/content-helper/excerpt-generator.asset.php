a5192a910c84ad2bc8c4d1fe2c080a13328d5d5e
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-04-18 09:49:54 +0300

    EmptyCredentialsMessage: Add fallback message

--- build/content-helper/excerpt-generator.asset.php
+++ build/content-helper/excerpt-generator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => '7d3cee1b5e317e421e5f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => 'aa0c1a70f7ee3d7a7a5e');
