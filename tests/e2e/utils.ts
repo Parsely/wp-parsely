b0306e0c06de1d6f1e6768bda086b54057e511a1
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-05-09 15:31:40 +0300

    Update wp-parsely version number to 3.14.5

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -8,7 +8,7 @@ import {
 	visitAdminPage,
 } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '3.14.4';
+export const PLUGIN_VERSION = '3.14.5';
 export const VALID_SITE_ID = 'demoaccount.parsely.com';
 export const INVALID_SITE_ID = 'invalid.parsely.com';
 export const VALID_API_SECRET = 'valid_api_secret';
