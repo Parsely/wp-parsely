4f510c2b16b0d23648c0cce9461020b57cabafe7
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-05-13 10:51:10 +0300

    Fix failing E2E test on GitHub - Attempt 2

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -154,7 +154,7 @@ export const getRelatedPostsMessage = async (
 	if ( ( await page.$( periodSettingSelector ) ) !== null ) {
 		// TODO: update this after the Related Posts panel revamp.
 		await page.select( periodSettingSelector, '30d' );
-		await page.waitForTimeout( 500 );
+		await page.waitForTimeout( 1000 );
 	}
 
 	setSidebarPanelExpanded( 'Related Posts', true );
