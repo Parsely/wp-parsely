a6b03141c0c1f325ca9a96cf21b81b518edf947d
Henrique Mouta <henrique@automattic.com>
2024-05-14 15:43:52 +0100

    Improve modal sidebar scrolling when navigating the smart links

--- src/content-helper/editor-sidebar/smart-linking/review-modal/component-sidebar.tsx
+++ src/content-helper/editor-sidebar/smart-linking/review-modal/component-sidebar.tsx
@@ -1,5 +1,7 @@
 import { MenuItem } from '@wordpress/components';
-import { useEffect, useState } from '@wordpress/element';
+import { useEffect, useRef } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { AiIcon } from '../../../common/icons/ai-icon';
 import { SmartLink } from '../provider';
 
 type ReviewModalSidebarProps = {
@@ -13,25 +15,63 @@ export const ReviewModalSidebar = ( {
 	links,
 	setSelectedLink,
 }: ReviewModalSidebarProps ): JSX.Element => {
+	const sidebarRef = useRef<HTMLDivElement>( null );
+	const itemRefs = useRef<( HTMLButtonElement | null )[]>( [] );
+
+	/**
+	 * Handles the scroll of the sidebar to the active link.
+	 */
+	useEffect( () => {
+		if ( activeLink ) {
+			const activeIndex = links?.findIndex( ( link ) => link.uid === activeLink.uid );
+			if ( activeIndex !== undefined && activeIndex !== -1 && sidebarRef.current && itemRefs.current[ activeIndex ] ) {
+				const sidebar = sidebarRef.current;
+				const activeItem = itemRefs.current[ activeIndex ];
+
+				if ( activeItem ) {
+					if ( activeIndex === 0 ) {
+						sidebar.scrollTop = 0;
+						return;
+					}
+					const sidebarRect = sidebar.getBoundingClientRect();
+					const activeItemRect = activeItem.getBoundingClientRect();
+
+					// Check if the active item is out of view.
+					if ( activeItemRect.top < sidebarRect.top || activeItemRect.bottom > sidebarRect.bottom ) {
+						// Scroll only if the active item is out of view.
+						sidebar.scrollTop = activeItem.offsetTop - sidebar.offsetTop;
+					}
+				}
+			}
+		}
+	}, [ activeLink, links ] );
+	const label = (
+		<span className="smart-linking-menu-label">
+			{ __( 'NEW', 'wp-parsely' ) }
+			<AiIcon />
+		</span>
+	);
+
 	return (
-		<div className="smart-linking-review-sidebar">
+		<div className="smart-linking-review-sidebar" ref={ sidebarRef }>
 			<div>
 				<div className="review-sidebar-header">
-					Outbound Smart Links
+					{ __( 'Outbound Smart Links', 'wp-parsely' ) }
 					<span>{ links?.length }</span>
 				</div>
-				{ links?.map( ( link ) => (
+				{ links?.map( ( link, index ) => (
 					<MenuItem
 						key={ link.uid }
+						ref={ ( el ) => itemRefs.current[ index ] = el }
 						className={ activeLink?.uid === link.uid ? 'is-selected' : '' }
 						role="menuitemradio"
-						shortcut={ ! link.applied ? 'NEW' : undefined }
 						isSelected={ activeLink?.uid === link.uid }
 						onClick={ () => {
 							setSelectedLink( link );
 						} }
 					>
-						{ link.text }
+						<span className="smart-linking-menu-item">{ link.text }</span>
+						{ ! link.applied && label }
 					</MenuItem>
 				) ) }
 			</div>
