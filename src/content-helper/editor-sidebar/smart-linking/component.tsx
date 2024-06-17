138c024d93b261966b0e47c1899610cac99d94e8
Henrique Mouta <henrique@automattic.com>
2024-06-17 10:18:07 +0100

    Rebuild assets

--- src/content-helper/editor-sidebar/smart-linking/component.tsx
+++ src/content-helper/editor-sidebar/smart-linking/component.tsx
@@ -9,7 +9,6 @@ import { select, useDispatch, useSelect } from '@wordpress/data';
 import { useEffect, useMemo, useState } from '@wordpress/element';
 import { __, _n, sprintf } from '@wordpress/i18n';
 import { Icon, external } from '@wordpress/icons';
-import { remove } from 'js-cookie';
 
 /**
  * Internal dependencies
