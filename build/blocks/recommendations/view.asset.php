32f5d3e861da05e9c139682c07cdc4bddfb4185d
Henrique Mouta <henrique@automattic.com>
2024-06-17 10:12:41 +0100

    Rebuild assets

--- build/blocks/recommendations/view.asset.php
+++ build/blocks/recommendations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '297b822893b9ed97ccb9');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '827c1c0c5b711b046baf');
