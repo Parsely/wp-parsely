0297c0bb2fd8cd4d7b821ae34bb9e77fadad54c1
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-06-26 17:31:37 +0300

    ContentHelperError: Avoid double error message prefix

--- build/content-helper/excerpt-generator.asset.php
+++ build/content-helper/excerpt-generator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => '1c19550f70ae7ceace8b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => 'f2a7e7c401d2876ef9ba');
