ab4c1748022b3307106dd81143525b07309c51d4
Henrique Mouta <henrique@automattic.com>
2024-07-05 15:36:07 +0100

    Fix issue when generating excerpt for empty content

--- src/content-helper/excerpt-generator/provider.ts
+++ src/content-helper/excerpt-generator/provider.ts
@@ -1,31 +1,41 @@
 /**
  * WordPress dependencies
  */
-import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal dependencies
  */
-import { ContentHelperError, ContentHelperErrorCode } from '../common/content-helper-error';
-
-/**
- * Specifies the form of the response returned by the
- * `/content-suggestions/suggest-brief` WordPress REST API endpoint.
- *
- * @since 3.13.0
- */
-interface ExcerptGeneratorApiResponse {
- error?: Error;
- data: string;
-}
+import { BaseProvider } from '../common/base-provider';
 
 /**
  * Provides the generate excerpt functionality to be used in other components.
  *
  * @since 3.13.0
  */
-export class ExcerptGeneratorProvider {
+export class ExcerptGeneratorProvider extends BaseProvider {
+	/**
+	 * The singleton instance of the ExcerptGeneratorProvider.
+	 *
+	 * @since 3.16.0
+	 */
+	private static instance: ExcerptGeneratorProvider;
+
+	/**
+	 * Returns the singleton instance of the TitleSuggestionsProvider.
+	 *
+	 * @since 3.16.0
+	 *
+	 * @return {ExcerptGeneratorProvider} The singleton instance.
+	 */
+	public static getInstance(): ExcerptGeneratorProvider {
+		if ( ! this.instance ) {
+			this.instance = new ExcerptGeneratorProvider();
+		}
+
+		return this.instance;
+	}
+
 	/**
 	 * Generates an excerpt for a given post.
 	 *
@@ -35,28 +45,18 @@ export class ExcerptGeneratorProvider {
 	 * @return {Promise<string>} The generated excerpt.
 	 */
 	public async generateExcerpt( title: string, content: string ): Promise<string> {
-		let response;
-		try {
-			response = await apiFetch<ExcerptGeneratorApiResponse>( {
-				method: 'POST',
-				path: addQueryArgs( '/wp-parsely/v1/content-suggestions/suggest-brief', {
-					title,
-				} ),
-				data: {
-					content,
-				},
-			} );
-		} catch ( wpError: any ) { // eslint-disable-line @typescript-eslint/no-explicit-any
-			return Promise.reject( new ContentHelperError( wpError.message, wpError.code ) );
+		if ( '' === title ) {
+			title = 'Untitled';
 		}
 
-		if ( response?.error ) {
-			return Promise.reject( new ContentHelperError(
-				response.error.message,
-				ContentHelperErrorCode.ParselyApiResponseContainsError
-			) );
-		}
-
-		return response?.data ?? '';
+		return await this.fetch<string>( {
+			method: 'POST',
+			path: addQueryArgs( '/wp-parsely/v1/content-suggestions/suggest-brief', {
+				title,
+			} ),
+			data: {
+				content,
+			},
+		} );
 	}
 }
