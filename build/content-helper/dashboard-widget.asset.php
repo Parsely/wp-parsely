5096e732a0ce4adfcdfa164eb748bc0a03688edc
Henrique Mouta <henrique@automattic.com>
2024-07-11 09:55:19 +0100

    Trying git merge with squash

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd02329b542f66b231abb');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cdeb35cda9a85c94d603');
