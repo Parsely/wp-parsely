945a362b8978d08613db739fbc24875b8f7e6b0f
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-08-26 18:42:58 +0300

    Prevent Jest from running Playwright tests

--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -3,5 +3,6 @@ const baseConfig = require( '@wordpress/scripts/config/jest-e2e.config' );
 
 module.exports = {
 	...baseConfig,
+	testMatch: [ '**/tests/e2e/specs/**/*.[jt]s?(x)' ], // Prevent running Playwright tests.
 	testTimeout: 35000, // Increased timeout for E2E tests.
 };
