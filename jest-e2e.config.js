4a19a33551a275a06c0fbb971175ce38d4f2d3f4
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-09-03 10:28:27 +0300

    Set Playwright E2E testing as the default

--- jest-e2e.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require( '@wordpress/scripts/config/jest-e2e.config' );
-
-module.exports = {
-	...baseConfig,
-	testMatch: [ '**/tests/e2e/specs/**/*.[jt]s?(x)' ], // Prevent running Playwright tests.
-	testTimeout: 35000, // Increased timeout for E2E tests.
-};
