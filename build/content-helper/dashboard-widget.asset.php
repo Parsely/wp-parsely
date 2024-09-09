800d4ac36eeca93bd11b2ba865c82bebe165ae21
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-09-09 09:41:38 +0300

    Fix linting issues

--- build/content-helper/dashboard-widget.asset.php
+++ build/content-helper/dashboard-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a741adf6df6b723b2f3d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'eef3b926e5f8ad8e3f11');
