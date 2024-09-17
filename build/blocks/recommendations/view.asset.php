b58e2f4193fd7dbee85a6dc81206574c61533fd5
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-09-17 15:03:52 +0300

    Rebuild assets

--- build/blocks/recommendations/view.asset.php
+++ build/blocks/recommendations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b6dc60cd3a86d4884489');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '816c6be0377185053443');
--- build/content-helper/excerpt-generator.asset.php
+++ build/content-helper/excerpt-generator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => '9db68c913fef0a3ad512');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url', 'wp-wordcount'), 'version' => '2a31635723a6474c1f34');
