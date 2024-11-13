81106d6367ce35f1eca252571ef81567bba53e58
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-11-13 15:38:52 +0200

    Add Traffic Boost settings infrastructure

--- src/UI/class-dashboard-page.php
+++ src/UI/class-dashboard-page.php
@@ -13,6 +13,7 @@ namespace Parsely\UI;
 use Parsely\Parsely;
 use Parsely\Permissions;
 use Parsely\Utils\Utils;
+use WP_REST_Request;
 
 use const Parsely\PARSELY_FILE;
 
@@ -25,6 +26,7 @@ final class Dashboard_Page {
 	/**
 	 * Instance of Parsely class.
 	 *
+	 * @since 3.18.0
 	 * @var Parsely
 	 */
 	private $parsely;
@@ -107,19 +109,6 @@ final class Dashboard_Page {
 	 */
 	public function add_dashboard_page_placeholder(): void {
 		echo '<div id="parsely-dashboard-page"></div>';
-
-		// TODO: The codeblock below is for demonstration purposes only and
-		// will be removed in the future.
-		if (
-			Permissions::current_user_can_use_pch_feature(
-				'traffic_boost',
-				$this->parsely->get_options()['content_helper']
-			)
-		) {
-			echo 'Traffic Boost is enabled.';
-		} else {
-			echo 'Traffic Boost is disabled.';
-		}
 	}
 
 	/**
@@ -168,11 +157,62 @@ final class Dashboard_Page {
 			true
 		);
 
+		// Inline scripts must be injected after enqueueing the main script.
+		$this->inject_content_helper_permissions();
+		$this->inject_traffic_boost_settings();
+
 		wp_enqueue_style(
 			'parsely-dashboard-page',
 			$built_assets_url . 'dashboard-page.css',
-			$asset_info['dependencies'],
+			array(),
 			$asset_info['version']
+		);
+	}
+
+	/**
+	 * Injects Content Helper permissions into the dashboard page.
+	 *
+	 * @since 3.18.0
+	 */
+	protected function inject_content_helper_permissions(): void {
+		$permissions_json = Permissions::get_pch_permissions_json(
+			$this->parsely->get_options()['content_helper']
+		);
+
+		wp_add_inline_script(
+			'parsely-dashboard-page',
+			"window.wpParselyContentHelperPermissions = '$permissions_json';",
+			'before'
+		);
+	}
+
+	/**
+	 * Injects Traffic Boost settings into the dashboard page.
+	 *
+	 * @since 3.18.0
+	 */
+	protected function inject_traffic_boost_settings(): void {
+		$settings = '';
+
+		if ( ! defined( 'INTEGRATION_TESTS_RUNNING' ) ) {
+			$settings = rest_do_request(
+				new WP_REST_Request(
+					'GET',
+					'/wp-parsely/v2/settings/traffic-boost'
+				)
+			)->get_data();
+		}
+
+		if ( ! is_array( $settings ) ) {
+			$settings = array();
+		}
+
+		$settings = wp_json_encode( $settings );
+
+		wp_add_inline_script(
+			'parsely-dashboard-page',
+			"window.wpParselyContentHelperSettings = '$settings';",
+			'before'
 		);
 	}
 }
--- src/content-helper/dashboard-page/pages/dashboard/page-component.tsx
+++ src/content-helper/dashboard-page/pages/dashboard/page-component.tsx
@@ -1,13 +1,72 @@
 /**
+ * Internal dependencies
+ */
+import { SettingsProvider, TrafficBoostSettings } from '../../../common/settings';
+import { getContentHelperPermissions } from '../../../common/utils/permissions';
+import { VerifyCredentials } from '../../../common/verify-credentials';
+
+/**
+ * Gets the settings from the passed JSON.
+ *
+ * If missing settings or invalid values are detected, they get set to their
+ * defaults.
+ *
+ * @since 3.18.0
+ *
+ * @param {string} settingsJson The JSON containing the settings.
+ *
+ * @return {TrafficBoostSettings} The resulting settings object.
+ */
+const getSettingsFromJson = ( settingsJson: string ): TrafficBoostSettings => {
+	// Default settings object.
+	const defaultSettings: TrafficBoostSettings = {
+		Setting1: 'Hello World!',
+	};
+
+	// If the settings are empty, try to get them from the global variable.
+	if ( '' === settingsJson ) {
+		settingsJson = window.wpParselyContentHelperSettings;
+	}
+
+	let parsedSettings: TrafficBoostSettings;
+
+	try {
+		parsedSettings = JSON.parse( settingsJson );
+	} catch ( e ) {
+		// Return defaults when parsing failed or the string is empty.
+		return defaultSettings;
+	}
+
+	// Merge parsed settings with default settings.
+	const mergedSettings = { ...defaultSettings, ...parsedSettings };
+
+	// Fix invalid values if any are found.
+	if ( typeof mergedSettings.Setting1 !== 'string' ) {
+		mergedSettings.Setting1 = defaultSettings.Setting1;
+	}
+
+	return parsedSettings;
+};
+
+/**
  * The main dashboard page component.
  *
  * @since 3.18.0
  */
 export const DashboardPage = () => {
 	return (
-		<>
-			<h1>Parse.ly Dashboard</h1>
-			<p>Welcome to the main Parse.ly dashboard page.</p>
-		</>
+		<SettingsProvider
+			endpoint="traffic-boost"
+			defaultSettings={ getSettingsFromJson( window.wpParselyContentHelperSettings ) }
+		>
+			<VerifyCredentials>
+				<>
+					<h1>Parse.ly</h1>
+					<p>Welcome to the Parse.ly Dashboard page!</p>
+					<p>Content Helper Permissions: { JSON.stringify( getContentHelperPermissions() ) }</p>
+					<p>Traffic Boost Settings: { JSON.stringify( getSettingsFromJson( window.wpParselyContentHelperSettings ) ) }</p>
+				</>
+			</VerifyCredentials>
+		</SettingsProvider>
 	);
 };
