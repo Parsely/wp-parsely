1e50213a174e8750b286e9cbebdab1ec82218c39
Henrique Mouta <henrique@automattic.com>
2024-11-15 12:50:38 +0000

    Add dashboard provider

---
+++ src/content-helper/dashboard-page/provider.tsx
@@ -0,0 +1,36 @@
+/**
+ * Internal dependencies
+ */
+import { BaseWordPressProvider } from '../common/base-wordpress-provider';
+
+/**
+ * DashboardProvider class for the plugin's dashboard.
+ *
+ * Extends the BaseWordPressProvider to inherit WordPress REST API functionalities.
+ *
+ * @since 3.18.0
+ */
+export class DashboardProvider extends BaseWordPressProvider {
+	/**
+	 * The singleton instance of the DashboardProvider.
+	 *
+	 * @since 3.18.0
+	 */
+	private static instance: DashboardProvider;
+
+	/**
+	 * Returns the singleton instance of the DashboardProvider.
+	 *
+	 * @since 3.18.0
+	 *
+	 * @return {DashboardProvider} The singleton instance.
+	 */
+	public static getInstance(): DashboardProvider {
+		if ( ! DashboardProvider.instance ) {
+			DashboardProvider.instance = new DashboardProvider();
+		}
+		return DashboardProvider.instance;
+	}
+
+	// Future dashboard-specific methods will be added here.
+}
