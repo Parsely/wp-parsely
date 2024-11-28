6cf559f640023abaf6629308374d512e2685a3b0
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-11-28 15:16:47 +0200

    Remove deprecated @types/wordpress__editor package

--- src/content-helper/editor-sidebar/excerpt-suggestions/component-panel.tsx
+++ src/content-helper/editor-sidebar/excerpt-suggestions/component-panel.tsx
@@ -20,7 +20,6 @@ import { ToneProp } from '../../common/components/tone-selector';
 /**
  * Internal dependencies
  */
-import { GutenbergFunction } from '../../../@types/gutenberg/types';
 import { Telemetry } from '../../../js/telemetry/telemetry';
 import {
 	ContentHelperError,
@@ -97,7 +96,7 @@ export const PostExcerptSuggestions = ( {
 
 	// Get the current excerpt, post content, and post title.
 	const { excerpt, postContent, postTitle } = useSelect( ( select ) => {
-		const { getEditedPostAttribute, getEditedPostContent } = select( editorStore ) as GutenbergFunction;
+		const { getEditedPostAttribute, getEditedPostContent } = select( editorStore );
 
 		let content = getEditedPostContent();
 		if ( ! content ) {
