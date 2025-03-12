a8e1a2347eb84b2b01f37f9f683e0b317e226851
Henrique Mouta <henrique@automattic.com>
2025-03-12 13:12:48 +0000

    Apply CodeRabbit suggestions

--- build/content-helper/dashboard-page.asset.php
+++ build/content-helper/dashboard-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'd08d0c20bc641da68ec5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '7dbaa5fab9ac1a5db182');
