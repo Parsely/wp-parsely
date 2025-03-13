c071271b5103144fc06cf8a4d9d1a50033793f7b
Henrique Mouta <henrique@automattic.com>
2025-03-13 09:51:48 +0000

    Disable the frontend preview while it's not ready for prime time.

--- build/content-helper/dashboard-page.asset.php
+++ build/content-helper/dashboard-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '7dbaa5fab9ac1a5db182');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'c7f1d7606b4f6af8291e');
