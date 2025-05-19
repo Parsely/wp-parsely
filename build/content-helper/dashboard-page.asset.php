03e8fddc343a52db19b7a80dd56962d0de2e50aa
Alec Geatches <alec.geatches@automattic.com>
2025-05-19 09:54:33 -0600

    Add a pointer state when hovering "Add Source Post" results

--- build/content-helper/dashboard-page.asset.php
+++ build/content-helper/dashboard-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'bcba05bc89eb39b41ee3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'c1d0bc77ba40114e2db1');
