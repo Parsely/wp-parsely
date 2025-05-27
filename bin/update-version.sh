73cd06a5e45fe155b33aeee9db08fd51045bdc42
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2025-05-27 10:38:19 +0300

    Fix update-version.sh

--- bin/update-version.sh
+++ bin/update-version.sh
@@ -11,8 +11,8 @@ set -e
 export LC_ALL=C
 
 if [ -z "$1" ]; then
-  echo "Error: You must specify a version number."
-  exit 1
+ echo "Error: You must specify a version number."
+ exit 1
 fi
 
 VERSION=$1
@@ -38,7 +38,7 @@ sed_inplace "s/Stable tag: .*  $/Stable tag: $VERSION  /" README.md
 sed_inplace "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
 sed_inplace "s/export const PLUGIN_VERSION = '.*'/export const PLUGIN_VERSION = '$VERSION'/" tests/e2e/utils.ts
 sed_inplace "s/ \* Version:           .*$/ \* Version:           $VERSION/" wp-parsely.php
-sed_inplace "s/const PARSELY_VERSION = '.*'/const PARSELY_VERSION = '$VERSION'/" wp-parsely.php
+sed_inplace "s/const PARSELY_VERSION             = '.*'/const PARSELY_VERSION             = '$VERSION'/" wp-parsely.php
 
 npm install --ignore-scripts # Update package-lock.json with the new version.
 
