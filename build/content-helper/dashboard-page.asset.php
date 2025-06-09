6062874800cd657b721c2d256bd752cc425ecf7f
Alec Geatches <alec.geatches@automattic.com>
2025-06-09 15:01:10 -0600

    Change hover text to "Highlight text on the page to customize link placement."

--- build/content-helper/dashboard-page.asset.php
+++ build/content-helper/dashboard-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '788add39cafa04b372d5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'cab5ce496bc812ce2a9a');
--- src/content-helper/dashboard-page/pages/traffic-boost/preview/components/preview-actions.tsx
+++ src/content-helper/dashboard-page/pages/traffic-boost/preview/components/preview-actions.tsx
@@ -135,7 +135,7 @@ export const PreviewActions = ( {
 								>{ __( 'Reject', 'wp-parsely' ) }</Button>
 								<VerticalDivider size={ 48 } color="#1e1e1e" />
 								{ ! selectedText &&
-									<Tooltip text={ __( 'Highlight text on the page to customize suggestion', 'wp-parsely' ) }>
+									<Tooltip text={ __( 'Highlight text on the page to customize link placement.', 'wp-parsely' ) }>
 										<div className="traffic-boost-preview-actions-hint">
 											<div className="traffic-boost-preview-actions-hint-text">
 												{ __( 'Highlight to customize', 'wp-parsely' ) }
