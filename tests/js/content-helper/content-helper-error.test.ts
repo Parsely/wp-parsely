79de9789d9d48e57f626ab8ff8d37a1c2f8170ff
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2025-06-20 16:32:20 +0300

    Update Content Helper error handling

--- tests/js/content-helper/content-helper-error.test.ts
+++ tests/js/content-helper/content-helper-error.test.ts
@@ -7,69 +7,101 @@ import {
 } from '../../../src/content-helper/common/content-helper-error';
 
 /**
+ * All the error codes of the ContentHelperErrorCode class.
+ */
+const errorCodes = [
+	ContentHelperErrorCode.AccessToFeatureDisabled,
+	ContentHelperErrorCode.FetchError,
+	ContentHelperErrorCode.HttpRequestFailed,
+	ContentHelperErrorCode.ParselyAborted,
+	ContentHelperErrorCode.ParselyApiForbidden,
+	ContentHelperErrorCode.ParselyApiResponseContainsError,
+	ContentHelperErrorCode.ParselyApiReturnedNoData,
+	ContentHelperErrorCode.ParselyApiReturnedTooManyResults,
+	ContentHelperErrorCode.PluginCredentialsNotSetMessageDetected,
+	ContentHelperErrorCode.PluginSettingsApiSecretNotSet,
+	ContentHelperErrorCode.PluginSettingsSiteIdNotSet,
+	ContentHelperErrorCode.PostIsNotPublished,
+	ContentHelperErrorCode.UnknownError,
+
+	// Suggestions API.
+	ContentHelperErrorCode.ParselySuggestionsApiAuthUnavailable,
+	ContentHelperErrorCode.ParselySuggestionsApiNoAuthentication,
+	ContentHelperErrorCode.ParselySuggestionsApiNoAuthorization,
+	ContentHelperErrorCode.ParselySuggestionsApiNoData,
+	ContentHelperErrorCode.ParselySuggestionsApiOpenAiError,
+	ContentHelperErrorCode.ParselySuggestionsApiOpenAiSchema,
+	ContentHelperErrorCode.ParselySuggestionsApiOpenAiUnavailable,
+	ContentHelperErrorCode.ParselySuggestionsApiSchemaError,
+];
+
+/**
+ * Errors that can be retried when encountered (soft errors).
+ */
+const softErrorCodes = [
+	ContentHelperErrorCode.FetchError,
+	ContentHelperErrorCode.HttpRequestFailed,
+	ContentHelperErrorCode.ParselySuggestionsApiAuthUnavailable,
+	ContentHelperErrorCode.ParselySuggestionsApiOpenAiError,
+	ContentHelperErrorCode.ParselySuggestionsApiOpenAiSchema,
+	ContentHelperErrorCode.ParselySuggestionsApiOpenAiUnavailable,
+	ContentHelperErrorCode.ParselySuggestionsApiSchemaError,
+];
+
+/**
+ * Verifies that all error codes within the ContentHelperErrorCode class are
+ * being tested.
+ *
+ * @since 3.21.0
+ */
+test( 'All Content Helper error codes are being tested', () => {
+	expect( Object.values( ContentHelperErrorCode ) ).toEqual( errorCodes );
+} );
+
+/**
  * Verifies that the retryFetch property of the ContentHelperErrorCode class
  * gets set to the correct value depending on the error code.
+ *
+ * @since 3.9.0
  */
 describe( 'ContentHelperError class should set retryFetch to', () => {
-	test( 'true when a query formulation error occurs', () => {
-		const code = ContentHelperErrorCode.CannotFormulateApiQuery;
-		expect( getRetryFetch( code ) ).toBe( true );
+	/**
+	 * Loops through all the error codes and checks if the retryFetch property
+	 * is set to true for soft errors and false for hard errors.
+	 *
+	 * @since 3.9.0
+	 * @since 3.21.0 Implemented looping instead of individual testing each error code.
+	 */
+	errorCodes.forEach( ( code ) => {
+		const expected = softErrorCodes.includes( code );
+
+		test( `${ expected } for ${ code }`, () => {
+			expect( getRetryFetch( code ) ).toBe( expected );
+		} );
 	} );
 
-	test( 'true for API fetch errors', () => {
-		const code = ContentHelperErrorCode.FetchError;
-		expect( getRetryFetch( code ) ).toBe( true );
-	} );
-
-	test( 'true for HTTP errors', () => {
-		const code = ContentHelperErrorCode.HttpRequestFailed;
-		expect( getRetryFetch( code ) ).toBe( true );
-	} );
-
-	test( 'false for API Forbidden errors', () => {
-		const code = ContentHelperErrorCode.ParselyApiForbidden;
+	/**
+	 * Returns the retryFetch property's value for a non-registered error code.
+	 *
+	 * Before version 3.21.0, this test would return true. Since version 3.21.0,
+	 * we handle non-registered error codes as hard errors that should return
+	 * false.
+	 *
+	 * @since 3.9.0
+	 * @since 3.21.0 Returns false instead of true for non-registered error codes.
+	 */
+	test( 'false when any other error occurs', () => {
+		const code = 'some_other_error_code' as ContentHelperErrorCode;
 		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'false when the API response contains an error', () => {
-		const code = ContentHelperErrorCode.ParselyApiResponseContainsError;
-		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'false when the API returns no data', () => {
-		const code = ContentHelperErrorCode.ParselyApiReturnedNoData;
-		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'false when the API returns too many results', () => {
-		const code = ContentHelperErrorCode.ParselyApiReturnedTooManyResults;
-		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'false when the API secret is not set', () => {
-		const code = ContentHelperErrorCode.PluginSettingsApiSecretNotSet;
-		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'false when the Site ID is not set', () => {
-		const code = ContentHelperErrorCode.PluginSettingsSiteIdNotSet;
-		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'false when the current post is not published', () => {
-		const code = ContentHelperErrorCode.PostIsNotPublished;
-		expect( getRetryFetch( code ) ).toBe( false );
-	} );
-
-	test( 'true when an unknown error occurs', () => {
-		const code = 'Any other error' as ContentHelperErrorCode;
-		expect( getRetryFetch( code ) ).toBe( true );
 	} );
 
 	/**
 	 * Returns the retryFetch property's value for the given error code.
 	 *
+	 * @since 3.9.0
+	 *
 	 * @param {ContentHelperErrorCode} code The error code to be examined.
+	 *
 	 * @return {boolean} The retryFetch property value.
 	 */
 	function getRetryFetch( code: ContentHelperErrorCode ): boolean {
