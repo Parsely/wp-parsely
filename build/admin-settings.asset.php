330e98f2b0a0a02c91fbcdad785caba12e3ed1ef
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2025-10-02 16:46:45 +0300

    Revert "Fix build error"

--- build/admin-settings.asset.php
+++ build/admin-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '906ad7f08aa99ffe61b2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '66f2b3e882defbfd9d95');
