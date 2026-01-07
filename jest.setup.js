dc6f0d507b32977d582f82a7b2f72aa941ccecdc
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2026-01-07 21:31:39 +0200

    Add TextEncoder global for Jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 global.TextDecoder = require( 'util' ).TextDecoder;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+global.TextEncoder = require( 'util' ).TextEncoder;
