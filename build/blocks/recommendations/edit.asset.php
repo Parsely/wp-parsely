901050e07900863a61839a0730c929b29499267a
Dhruv Bhanushali <dhruv_b@live.com>
2026-04-22 00:14:15 +0530

    Update `LeafIcon` to use standard icon viewbox and path dimensions

--- build/blocks/recommendations/edit.asset.php
+++ build/blocks/recommendations/edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8c95e0e28b467492e414');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '64ae9e2aa83b04264347');
